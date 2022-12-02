f5443713cd34ad284bb01d4bb4ea1d23bb3e43d2
Alvaro <alvaro.saburido@gmail.com>
2022-12-02 08:10:12 +0100

    fix(core): enabled function calling on process props

--- packages/tres/src/App.vue
+++ packages/tres/src/App.vue
@@ -1,23 +1,19 @@
 <script setup lang="ts">
-import { unref } from 'vue'
 import { Color } from 'three'
-import { useTweakPane } from '@tresjs/cientos'
-import { useTres } from './core'
+import { useTweakPane, OrbitControls } from '../../cientos/src'
 import TestSphere from '/@/components/TestSphere.vue'
 
 const colorTeal = new Color('teal')
 useTweakPane()
-
-const { state } = useTres()
 
 const meshPosition = [0, 1, 0]
 </script>
 <template>
   <Suspense>
     <TresCanvas shadows alpha power-preference="high-performance" preserve-drawing-buffer physically-correct-lights>
-      <TresPerspectiveCamera :position="[0, 5, 5]" :fov="45" :near="0.1" :far="1000" />
+      <TresPerspectiveCamera :position="[5, 5, 5]" :fov="45" :near="0.1" :far="1000" :look-at="[-8, 3, -3]" />
       <TresScene>
-        <TresOrbitControls v-if="state.renderer" :args="[unref(state), state.renderer?.domElement]" />
+        <OrbitControls />
         <TresMesh :position="meshPosition" :scale="1">
           <TresSphereGeometry />
           <TresMeshToonMaterial :color="colorTeal" />
