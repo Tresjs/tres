765a01faf7c35ba60cfbd5ea916b6c3a93bd454e
Alvaro <alvaro.saburido@gmail.com>
2023-01-04 07:32:51 +0100

    docs: added love vue threejs animation to hero

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,16 @@
 import Tres from '@tresjs/core'
+import 'uno.css'
 // .vitepress/theme/index.ts
 import DefaultTheme from 'vitepress/theme'
 import './config.css'
 import FirstScene from './components/FirstScene.vue'
 import StackBlitzEmbed from './components/StackBlitzEmbed.vue'
+import LoveVueThreejs from './components/LoveVueThreejs.vue'
+import TresLayout from './TresLayout.vue'
 
 export default {
   ...DefaultTheme,
+
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp(ctx)
     ctx.app.component('FirstScene', FirstScene)
@@ -17,6 +21,11 @@ export default {
       // ... server only logic
       ctx.app.use(Tres)
     }
-    console.log(ctx)
   },
+  Layout: TresLayout,
+  /* Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'home-hero-image': () => h('div', null, 'Hello'),
+    })
+  }, */
 }
