14ab727685bcab4fa78addd620f1652700ca5613
Alvaro <alvaro.saburido@gmail.com>
2023-01-17 13:22:52 +0100

    fix(core): remove histoire

--- packages/tres/histoire.setup.ts
+++
@@ -1,7 +0,0 @@
-import { defineSetupVue3 } from '@histoire/plugin-vue'
-
-import Tres from '@tresjs/core'
-
-export const setupVue3 = defineSetupVue3(({ app }) => {
-  app.use(Tres)
-})
--- packages/tres/src/examples/models/gltf/AkuAkuCientos.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import { useGLTF } from '@tresjs/cientos'
-const { scene } = await useGLTF('/models/AkuAku.gltf')
-</script>
-<template>
-  <TresMesh v-bind="scene" />
-</template>
