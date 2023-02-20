aaa41a86854ef24c6165ccf6f078d05d5635c4cc
alvarosabu <alvaro.saburido@gmail.com>
2023-02-20 08:27:59 +0100

    feat: realistic pokeball

---
+++ src/components/realistic/TheExperience.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Environment, OrbitControls } from '@tresjs/cientos'
+import { shallowRef, watch } from 'vue'
+import ThePokeball from './ThePokeball.vue'
+
+const environmentTexture = shallowRef()
+
+const environmentFiles = ['/px.jpg', '/nx.jpg', '/py.jpg', '/ny.jpg', '/pz.jpg', '/nz.jpg']
+</script>
+
+<template>
+  <Suspense>
+    <TresCanvas preset="realistic">
+      <OrbitControls />
+      <TresPerspectiveCamera :position="[11, 11, 11]" />
+      <TresScene>
+        <Environment
+          background
+          :files="environmentFiles"
+          :path="'https://raw.githubusercontent.com/Tresjs/assets/main/textures/environmentMap'"
+        />
+        <ThePokeball />
+        <TresAmbientLight :intensity="1" />
+        <TresDirectionalLight :position="[5, 5, 5]" :intensity="1" />
+      </TresScene>
+    </TresCanvas>
+  </Suspense>
+</template>
---
+++ src/components/realistic/ThePokeball.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { FrontSide } from 'three';
+import { shallowRef, watch } from 'vue';
+import { GLTFModel } from '@tresjs/cientos'
+
+
+const gameboyRef = shallowRef()
+
+watch(gameboyRef, ({ getModel }) => {
+  const model = getModel()
+
+  model.scale.set(0.4, 0.4, 0.4)
+
+})
+</script>
+<template>
+ <GLTFModel
+  ref="gameboyRef"
+  path="https://raw.githubusercontent.com/Tresjs/assets/main/models/gltf/realistic-pokeball/scene.gltf"
+  draco
+/>
+</template>
