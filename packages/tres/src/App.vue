65629e7ed7963e93bfe85becb62ebe74d6773fb6
alvarosabu <alvaro.saburido@gmail.com>
2023-02-23 20:48:44 +0100

    feat(core): basic unit test for raycasters

--- packages/tres/src/App.vue
+++ packages/tres/src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import { CameraType, useCamera } from './core'
 import TheEnvironment from '/@/components/TheEnvironment.vue'
 </script>
 
 <template>
   <Suspense>
-    <!--   <TheEnvironment /> -->
+    <TheEnvironment />
   </Suspense>
 </template>
--- packages/tres/vite.config.ts
+++ packages/tres/vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
     Inspect(),
   ],
   test: {
-    environment: 'happy-dom',
+    environment: 'jsdom',
     globals: true,
     threads: false,
     alias: {
