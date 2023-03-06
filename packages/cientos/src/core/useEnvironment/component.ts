b8c436d94a9f543923ada898340303b5076d446f
alvarosabu <alvaro.saburido@gmail.com>
2023-03-06 21:57:01 +0100

    feat(cientos): better typ support useEnvironment

--- packages/cientos/src/core/useEnvironment/component.ts
+++ packages/cientos/src/core/useEnvironment/component.ts
@@ -1,27 +1,13 @@
-import { EnvironmentOptions, EnvironmentPresetsType } from './const'
-import { CubeTexture, Texture, TextureEncoding } from 'three'
-import { defineComponent, PropType } from 'vue'
+import { EnvironmentOptions } from './const'
+import { CubeTexture, Texture } from 'three'
+import { defineComponent } from 'vue'
 
 import { useEnvironment } from '.'
 
-export const Environment = defineComponent({
+export const Environment = defineComponent<EnvironmentOptions>({
   name: 'Environment',
-  props: {
-    background: {
-      type: Boolean,
-      default: false,
-    },
-    blur: {
-      type: Number,
-      default: 0,
-    },
-    files: {
-      type: [String, Array],
-    },
-    encoding: Object as PropType<TextureEncoding>,
-    path: String,
-    preset: Object as PropType<EnvironmentPresetsType>,
-  },
+  props: ['background', 'blur', 'files', 'encoding', 'path', 'preset'] as unknown as undefined,
+
   async setup(props, { expose }) {
     let texture: Texture | CubeTexture | null = null
 
