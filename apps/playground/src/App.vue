c878635afaf4bf9c4537ee54057a01ae5e357f48
alvarosabu <alvaro.saburido@gmail.com>
2023-03-16 11:08:46 +0100

    chore: add playground

---
+++ apps/playground/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+</script>
+
+<template>
+  <TresCanvas> </TresCanvas>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
