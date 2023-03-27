2b7299fabe2f0228ed24a62d52520456a9f91b25
alvarosabu <alvaro.saburido@gmail.com>
2023-03-27 22:22:33 +0200

    feat: basic experiment metadata

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,13 +1,77 @@
 <script setup lang="ts">
-import { useTres } from '@tresjs/core'
-
 definePageMeta({
   layout: 'experiment',
 })
 const { path } = useRoute()
-
 const { data } = await useAsyncData(`content-${path}`, () => {
   return queryContent().where({ _path: path }).findOne()
+})
+
+console.log(data)
+
+useHead({
+  title: `${data?.value.title} - Tres`,
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: data?.value.description,
+    },
+    {
+      hid: 'keywords',
+      property: 'keywords',
+      keywords: data?.value.tags?.join(', '),
+    },
+    // og
+    {
+      hid: 'og:description',
+      property: 'og:description',
+      content: data?.value.description,
+    },
+    {
+      hid: 'og:title',
+      property: 'og:title',
+      content: `${data?.value.title} - Tres`,
+    },
+    {
+      hid: 'og:type',
+      property: 'og:type',
+      content: 'project',
+    },
+    {
+      hid: 'og:image',
+      property: 'og:image',
+      content: data?.value.thumbnail,
+    },
+    {
+      hid: 'og:image:alt',
+      property: 'og:image:alt',
+      content: data?.value.title,
+    },
+    // Twitter
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:site', content: '@alvarosabu' },
+    {
+      hid: 'twitter:title',
+      property: 'twitter:title',
+      content: `${data?.value.title} - Tres`,
+    },
+    {
+      hid: 'twitter:description',
+      name: 'twitter:description',
+      content: data?.value.description,
+    },
+    {
+      hid: 'twitter:image',
+      name: 'twitter:image',
+      content: data?.value.thumbnail,
+    },
+    {
+      hid: 'twitter:image:alt',
+      name: 'twitter:image:alt',
+      content: data?.value.title,
+    },
+  ],
 })
 </script>
 <template>
