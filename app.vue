5b884bac9ae187fe6e6093aa48e444cf376e13fe
alvarosabu <alvaro.saburido@gmail.com>
2023-03-28 06:26:17 +0200

    chore: fixing netlify build

--- app.vue
+++ app.vue
@@ -1,10 +1,3 @@
-<script lang="ts">
-import { extend } from '@tresjs/core'
-import { OrbitControls } from 'three-stdlib'
-
-extend({ OrbitControls })
-</script>
-
 <template>
   <div>
     <NuxtLayout>
