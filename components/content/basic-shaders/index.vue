d453dd6972c44c5020a91a6d73f166b539c1513c
alvarosabu <alvaro.saburido@gmail.com>
2023-04-03 09:52:57 +0200

    feat: basic shaders

---
+++ components/content/basic-shaders/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { Vector2 } from 'three'
+import { TresCanvas, TresInstance, useRenderLoop } from '@tresjs/core'
+import { OrbitControls, useTweakPane } from '@tresjs/cientos'
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
+
+const blobRef: Ref<TresInstance | null> = ref(null)
+const materialRef: Ref<TresInstance | null> = ref(null)
+const uniforms = {
+  uTime: { value: 0 },
+  uAmplitude: { value: new Vector2(0.1, 0.1) },
+  uFrequency: { value: new Vector2(20, 5) },
+}
+
+useTweakPane()
+
+const { onLoop, resume } = useRenderLoop()
+resume()
+onLoop(({ _delta, elapsed }) => {
+  if (blobRef.value) {
+    blobRef.value.material.uniforms.uTime.value = elapsed
+  }
+})
+</script>
+
+<template>
+  <TresCanvas clear-color="#111" shadows alpha>
+    <TresPerspectiveCamera :position="[11, 11, 11]" />
+    <OrbitControls />
+    <TresMesh ref="blobRef" :position="[0, 4, 0]">
+      <TresSphereGeometry :args="[2, 32, 32]" />
+      <TresShaderMaterial
+        ref="materialRef"
+        :vertex-shader="vertexShader"
+        :fragment-shader="fragmentShader"
+        :uniforms="uniforms"
+      />
+    </TresMesh>
+    <TresMesh :rotation="[-Math.PI / 2, 0, 0]">
+      <TresPlaneGeometry :args="[10, 10, 10, 10]" />
+      <TresMeshBasicMaterial color="#444" />
+    </TresMesh>
+  </TresCanvas>
+</template>
---
+++ content/experiments/basic-shaders.md
@@ -0,0 +1,10 @@
+---
+thumbnail: /basic-shaders.png
+title: Basic Shaders
+slug: basic-shaders
+author: alvarosabu
+description: Demo of using shaders with TresJS
+tags: ['basic', 'shaders', 'glsl']
+---
+
+<BasicShaders />
