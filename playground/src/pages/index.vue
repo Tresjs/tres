c71c781a9d6c9699a5a3ad86ab4e8ac78077cde7
alvarosabu <alvaro.saburido@gmail.com>
2023-04-07 11:26:11 +0200

    chore: added separate demo for PointerLock

--- playground/src/pages/index.vue
+++ playground/src/pages/index.vue
@@ -1,23 +1,5 @@
-<script setup lang="ts">
-import { TresCanvas } from '@tresjs/core'
-import { PointerLockControls } from '@cientos'
-import { BasicShadowMap, sRGBEncoding, NoToneMapping } from 'three'
-
-const gl = {
-  clearColor: '#82DBC5',
-  shadows: true,
-  alpha: false,
-  shadowMapType: BasicShadowMap,
-  outputEncoding: sRGBEncoding,
-  toneMapping: NoToneMapping,
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <TresCanvas v-bind="gl">
-    <TresPerspectiveCamera :position="[3, 3, 3]" />
-    <PointerLockControls selector="lock" />
-    <TresGridHelper :args="[10, 10]" />
-    <TresAmbientLight :intensity="1" />
-  </TresCanvas>
+  <PointerLockControlsDemo />
 </template>
