297cfe47a4f68fa8944428c734dcb72a0789ad8d
alvarosabu <alvaro.saburido@gmail.com>
2023-04-07 11:01:57 +0200

    chore: moved pointer move test to the correct playground scene

--- playground/src/components/TheFirstScene.vue
+++ playground/src/components/TheFirstScene.vue
@@ -1,22 +1,11 @@
 <script setup lang="ts">
 import { TresCanvas } from '/@/'
-
-const context = ref()
-
-watchEffect(() => {
-  console.log(context)
-})
-const toLog = (value:any) => {
-  console.log('click', value)
-}
 </script>
 
 <template>
   <TresCanvas ref="context" clear-color="#82DBC5" window-size>
     <TresPerspectiveCamera />
-    <TresMesh 
-    @pointer-move="(ev) => toLog(ev)"
-    >
+    <TresMesh>
       <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
       <TresMeshBasicMaterial color="orange" />
     </TresMesh>
