9e174eb9c5fbce6e1ccb2a906ae7ffd6b1b4c8b0
alvarosabu <alvaro.saburido@gmail.com>
2023-04-25 07:34:32 +0200

    fix: check if removing .nuxt remove tracking

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default" | "experiment"
-declare module "/Users/alvarosabu/Projects/tres/playground/node_modules/.pnpm/nuxt@3.4.1_@types+node@18.15.11/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "/Users/alvarosabu/Projects/tres/playground/node_modules/.pnpm/nuxt@3.4.1_@types+node@18.15.11/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
