4eb6e0086c05807bc95b64d1a5eab3a2ac20ca76
alvarosabu <alvaro.saburido@gmail.com>
2023-05-01 19:12:31 +0200

    feat: fancy checkbox

--- src/components/BooleanControl.vue
+++ src/components/BooleanControl.vue
@@ -7,10 +7,14 @@ defineProps<{
 <template>
   <div class="flex justify-start gap-2 items-start mb-2">
     <label class="text-gray-500 w-1/3">{{ label }}</label>
-    <input
-      v-model="control.value"
-      class="text-left text-gray-500 bg-transparent border-b-2 border-gray-400 focus:border-gray-500"
-      type="checkbox"
-    />
+    <input :id="control.label" v-model="control.value" class="hidden" type="checkbox" />
+    <label :for="control.label" class="inline-flex items-center cursor-pointer">
+      <span
+        :class="{ 'bg-dark-500': control.value, 'bg-gray-200': !control.value }"
+        class="w-4 h-4 flex justify-center items-center rounded border border-gray-300 mr-2 transition-colors duration-200"
+      >
+        <i v-show="control.value" class="i-ic:baseline-check text-light"></i
+      ></span>
+    </label>
   </div>
 </template>
