5ede6be70cc98ac9d93df212091f58afc890ed19
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2023-05-03 15:13:30 -0400

    docs: add mapControls docs, update pointer lock docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -40,6 +40,7 @@ export default defineConfig({
           { text: 'OrbitControls', link: '/guide/controls/orbit-controls' },
           { text: 'TransformControls', link: '/guide/controls/transform-controls' },
           { text: 'PointerLockControls', link: '/guide/controls/pointer-lock-controls' },
+          { text: 'MapControls', link: '/guide/controls/map-controls' },
         ],
       },
       {
