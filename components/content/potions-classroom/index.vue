f1b18a390256172e9eeb9069f81cb5bc00b7d796
alvarosabu <alvaro.saburido@gmail.com>
2023-05-05 17:50:20 +0200

    feat: potions classroom metadata

--- components/content/potions-classroom/index.vue
+++ components/content/potions-classroom/index.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core'
-import { BasicShadowMap } from 'three'
-import { OrbitControls, useTweakPane } from '@tresjs/cientos'
-import { EffectComposer, Bloom } from '@tresjs/post-processing'
+import { BasicShadowMap, DefaultLoadingManager } from 'three'
+import { useTweakPane } from '@tresjs/cientos'
 
 const gl = reactive({
   clearColor: '#242424',
@@ -17,19 +16,38 @@ const { pane } = useTweakPane()
 
 pane.addInput(gl, 'clearColor', { label: 'Background' })
 
-const bloomParams = reactive({
-  luminanceThreshold: 0.1,
-  luminanceSmoothing: 0.3,
-  mipmapBlur: true,
-  intensity: 4.0,
-  radius: 0.85,
-})
+const hasFinishLoading = ref(false)
+const progress = ref(0)
+
+let saveLastTotalLoaded = 0
+
+DefaultLoadingManager.onProgress = (item, loaded, total) => {
+  if (loaded === total) {
+    saveLastTotalLoaded = total
+    hasFinishLoading.value = true
+  }
+  progress.value = Math.round(((loaded - saveLastTotalLoaded) / (total - saveLastTotalLoaded)) * 100 || 100, 2)
+}
 </script>
 
 <template>
+  <Transition
+    name="fade-overlay"
+    enter-active-class="opacity-1 transition-opacity duration-200"
+    leave-active-class="opacity-0 transition-opacity duration-200"
+  >
+    <div
+      v-show="!hasFinishLoading"
+      class="absolute bg-grey-600 t-0 l-0 w-full h-full z-20 flex justify-center items-center text-black font-mono"
+    >
+      <div class="w-200px text-white">
+        Loading... {{ progress }} %
+        <i class="i-game-icons-snitch-quidditch-ball animate-tada text-yellow"></i>
+      </div>
+    </div>
+  </Transition>
   <TresCanvas v-bind="gl">
-    <TresPerspectiveCamera :position="[11, 15, 11]" :look-at="[0, 8, 0]" />
-    <OrbitControls />
+    <TresPerspectiveCamera :position="[11, 15, 11]" :look-at="[0, 4, 0]" />
     <Suspense>
       <Experience />
     </Suspense>
