e8e91749ca16364ff0f9c2c477506d3db0dfb597
alvarosabu <alvaro.saburido@gmail.com>
2023-06-20 21:21:35 +0200

    fix: nuxt stones scene

--- components/content/nuxt-stones/index.vue
+++ components/content/nuxt-stones/index.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
-import { TresCanvas } from '@tresjs/core'
 import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three'
 
-import { OrbitControls, useTweakPane } from '@tresjs/cientos'
-import { EffectComposer, Bloom } from '@tresjs/post-processing'
-
 import Stones from './Stones.vue'
-import Floor from './Floor.vue'
 
 const gl = {
   clearColor: '#18181B',
@@ -17,23 +12,13 @@ const gl = {
   toneMapping: NoToneMapping,
 }
 
-const bloomParams = reactive({
-  luminanceThreshold: 0.2,
-  luminanceSmoothing: 0.3,
-  mipmapBlur: true,
-  intensity: 0.5,
-})
 </script>
 
 <template>
   <TresCanvas v-bind="gl">
     <TresPerspectiveCamera :position="[-5.3, 8.3, 10.6]" :look-at="[0, 0, 0]" />
     <OrbitControls />
-    <Suspense>
-      <EffectComposer :depth-buffer="true">
-        <Bloom v-bind="bloomParams"></Bloom>
-      </EffectComposer>
-    </Suspense>
+    <EffectLayer />
     <Suspense>
       <Stones />
     </Suspense>
