3ab2265b67a94c18fc576e06dac44603d511e074
Alvaro Saburido <alvaro.saburido@gmail.com>
2023-06-26 09:19:22 +0200

    docs: add core team page (#319)

---
+++ docs/_data/team.js
@@ -0,0 +1,36 @@
+export const core = [
+  {
+    avatar: 'https://www.github.com/alvarosabu.png',
+    name: 'Alvaro Sabu',
+    title: 'Creative Engineer | Content Creator',
+    org: 'TresJS',
+    orgLink: 'https://tresjs.org/',
+    desc: 'TresJS author, DevRel Engineer at @storyblok',
+    links: [
+      { icon: 'github', link: 'https://github.com/alvarosabu' },
+      { icon: 'twitter', link: 'https://twitter.com/alvarosabu' },
+      { icon: 'youtube', link: 'https://www.youtube.com/channel/UC6D2KveNVcuuPqOKp0YWO3w' },
+    ],
+    sponsor: 'https://github.com/sponsors/alvarosabu',
+  },
+  {
+    avatar: 'https://www.github.com/JaimeTorrealba.png',
+    name: 'Jaime Torrealba',
+    title: 'Frontend Engineer',
+    desc: 'A passionate creative-developer',
+    links: [
+      { icon: 'github', link: 'https://github.com/JaimeTorrealba' },
+      { icon: 'twitter', link: 'https://twitter.com/jaimebboyjt' },
+    ],
+  },
+  {
+    avatar: 'https://www.github.com/Tinoooo.png',
+    name: 'Tino Koch',
+    title: 'Frontend Engineer',
+    desc: 'A passionate creative-developer',
+    links: [
+      { icon: 'github', link: 'https://github.com/Tinoooo' },
+      { icon: 'twitter', link: 'https://twitter.com/@ichbintino' },
+    ],
+  },
+]
