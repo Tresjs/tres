8646ec7ff9333073eb73bec50b8029bbda936ee8
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2023-09-16 11:36:10 -0300

    chore: remove the instance in unmounted

--- src/core/directives/vLightHelper.ts
+++ src/core/directives/vLightHelper.ts
@@ -12,6 +12,15 @@ export const vLightHelper = {
     const currentHelper = helpers[el.type]
     el.parent.add(new currentHelper(el))
   },
+  unmounted: (el: any) => {
+    if (!el.isLight) {
+      logWarning(`${el.type} is not a light`)
+      return
+    }
+    const currentHelper = helpers[el.type]
+    const currentInstance = el.parent.children.find((child: any) => child instanceof currentHelper)
+    el.parent.remove(currentInstance)
+  },
 }
 
 const helpers = {
