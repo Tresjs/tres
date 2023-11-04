1c067ffa2b1c56c367cc0b462e3d6e8b050f5b14
alvarosabu <alvaro.saburido@gmail.com>
2023-11-04 12:09:15 +0100

    feat: add vector 2 support

--- playground/src/pages/folders.vue
+++ playground/src/pages/folders.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core'
-import { BasicShadowMap, SRGBColorSpace, NoToneMapping, Vector3 } from 'three'
+import { BasicShadowMap, SRGBColorSpace, NoToneMapping, Vector3, Vector2 } from 'three'
 
 import { OrbitControls, Sphere } from '@tresjs/cientos'
 
@@ -17,12 +17,13 @@ const gl = {
 }
 
 const { position } = useControls('camera', {
-  position: new Vector3(3, 3, 3),
+  position: new Vector3(3, 2, 3),
+  /*  delay: new Vector2(1.5, 3.5), */
 })
 
-const { position: lightPosition } = useControls('light', {
+/* const { position: lightPosition } = useControls('light', {
   position: new Vector3(3, 3, 3),
-})
+}) */
 </script>
 
 <template>
@@ -36,10 +37,10 @@ const { position: lightPosition } = useControls('light', {
     <Sphere>
       <TresMeshToonMaterial />
     </Sphere>
-    <TresDirectionalLight
+    <!-- <TresDirectionalLight
       :args="[0xffffff, 1]"
       :position-x="lightPosition.x"
-    />
+    /> -->
     <TresAmbientLight :intensity="1" />
   </TresCanvas>
 </template>
--- src/utils/is.ts
+++ src/utils/is.ts
@@ -1,5 +1,5 @@
-export function isVector2(obj: { x: number; y: number; z: number }) {
-  return obj && typeof obj.x === 'number' && typeof obj.y === 'number' && typeof obj.z === 'undefined'
+export function isVector2(obj: { x: number; y: number }) {
+  return obj && typeof obj.x === 'number' && typeof obj.y === 'number'
 }
 
 export function isVector3(obj: { x: number; y: number; z: number }) {
