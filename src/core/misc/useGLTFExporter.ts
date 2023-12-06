7deeb74a0380b6faacd4481996d77cfe46c8b47a
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2023-12-06 06:50:08 -0300

    chore: fix types

--- src/core/misc/useGLTFExporter.ts
+++ src/core/misc/useGLTFExporter.ts
@@ -15,42 +15,45 @@ interface gltfExporterOptions {
   includeCustomExtensions?: boolean
 }
 
-export async function useGLTFExporter(object3D: Object3D, options?: gltfExporterOptions ) {
+export async function useGLTFExporter(
+  object3D: Object3D,
+  options?: gltfExporterOptions,
+) {
   const exporter = new GLTFExporter()
   const objectToDownload = ref(object3D)
   const name = options?.fileName || object3D.name || 'scene'
 
   exporter.parse(
     objectToDownload.value,
-    ( gltf: any ) => {
-      if ( gltf instanceof ArrayBuffer ) {
-        saveArrayBuffer( gltf, `${name}.glb` )
+    (gltf) => {
+      if (gltf instanceof ArrayBuffer) {
+        saveArrayBuffer(gltf, `${name}.glb`)
       }
       else {
-        const output = JSON.stringify( gltf, null, 2 )
-        saveString( output, `${name}.gltf` )
+        const output = JSON.stringify(gltf, null, 2)
+        saveString(output, `${name}.gltf`)
       }
     },
-    ( error: any ) => {
-      logError( 'An error happened while exporting the GLTF', error )
+    (error: any) => {
+      logError('An error happened while exporting the GLTF', error)
     },
     options,
   )
 }
 
-function saveString( text: string, filename: string ) {
-  save( new Blob( [ text ], { type: 'text/plain' } ), filename )
+function saveString(text: string, filename: string) {
+  save(new Blob([text], { type: 'text/plain' }), filename)
 }
 
-function saveArrayBuffer( buffer: any, filename: string ) {
-  save( new Blob( [ buffer ], { type: 'application/octet-stream' } ), filename )
+function saveArrayBuffer(buffer: any, filename: string) {
+  save(new Blob([buffer], { type: 'application/octet-stream' }), filename)
 }
 
-function save( blob: Blob, filename: string ) {
-  const link = document.createElement( 'a' )
+function save(blob: Blob, filename: string) {
+  const link = document.createElement('a')
   link.style.display = 'none'
-  document.body.appendChild( link )
-  link.href = URL.createObjectURL( blob )
+  document.body.appendChild(link)
+  link.href = URL.createObjectURL(blob)
   link.download = filename
   link.click()
   link.remove()
