f5927dbbad90a7d5f24a9cb2c13dc1b5fc05d63b
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2023-12-25 19:55:16 -0300

    docs: add docs

---
+++ docs/.vitepress/theme/components/BakeShadowsDemo.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { shallowRef } from 'vue'
+import { BakeShadows } from '@tresjs/cientos'
+import { TresCanvas, useRenderLoop } from '@tresjs/core'
+
+const cubeRef = shallowRef()
+
+const { onLoop } = useRenderLoop()
+
+onLoop(({ elapsed }) => {
+  if (cubeRef.value) {
+    cubeRef.value.rotation.y = elapsed * 0.5
+    cubeRef.value.rotation.x = elapsed * 0.5
+  }
+})
+</script>
+
+<template>
+  <TresCanvas
+    clear-color="#82DBC5"
+    shadows
+  >
+    <TresPerspectiveCamera
+      :position="[0, 2, 5]"
+      :look-at="[0, 0, 0]"
+    />
+    <BakeShadows />
+    <TresMesh
+      ref="cubeRef"
+      cast-shadow
+    >
+      <TresBoxGeometry />
+      <TresMeshStandardMaterial :color="0x00ff00" />
+    </TresMesh>
+    <TresMesh
+      receive-shadow
+      :position="[0, -2, 0]"
+      :rotation-x="-Math.PI / 2"
+    >
+      <TresPlaneGeometry :args="[5, 5]" />
+      <TresMeshStandardMaterial :color="0xf7f7f7" />
+    </TresMesh>
+    <TresDirectionalLight
+      cast-shadow
+      :position="[0, 10, 0]"
+    />
+  </TresCanvas>
+</template>
