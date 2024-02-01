9f3710ed9c1c24c3a37d65c10f8dd394b74fe671
Peter <petermoldovia@yahoo.ca>
2024-02-01 19:03:29 +0100

    feat(Camera): add 'manual' flag to camera

--- src/composables/useCamera/index.ts
+++ src/composables/useCamera/index.ts
@@ -4,23 +4,19 @@ import { Camera, OrthographicCamera, PerspectiveCamera } from 'three'
 import type { TresScene } from '../../types'
 import type { TresContext } from '../useTresContextProvider'
 
-export const useCamera = ({ sizes, scene }: Pick<TresContext, 'sizes'> & { scene: TresScene }) => {
-
+export const useCamera = ({
+  sizes,
+  scene,
+}: Pick<TresContext, 'sizes'> & { scene: TresScene }) => {
   // the computed does not trigger, when for example the camera position changes
   const cameras = ref<Camera[]>([])
-  const camera = computed<Camera | undefined>(
-    () => cameras.value[0],
-  )
+  const camera = computed<Camera | undefined>(() => cameras.value[0])
 
   const registerCamera = (newCamera: Camera, active = false) => {
-    if (cameras.value.some(({ uuid }) => uuid === newCamera.uuid))
-      return
+    if (cameras.value.some(({ uuid }) => uuid === newCamera.uuid)) return
 
-    if (active)
-      setCameraActive(newCamera)
-    else
-      cameras.value.push(newCamera)
-
+    if (active) setCameraActive(newCamera)
+    else cameras.value.push(newCamera)
   }
 
   const deregisterCamera = (camera: Camera) => {
@@ -28,24 +24,38 @@ export const useCamera = ({ sizes, scene }: Pick<TresContext, 'sizes'> & { scene
   }
 
   const setCameraActive = (cameraOrUuid: string | Camera) => {
-    const camera = cameraOrUuid instanceof Camera
-      ? cameraOrUuid
-      : cameras.value.find((camera: Camera) => camera.uuid === cameraOrUuid)
+    const camera
+      = cameraOrUuid instanceof Camera
+        ? cameraOrUuid
+        : cameras.value.find((camera: Camera) => camera.uuid === cameraOrUuid)
 
     if (!camera) return
 
-    const otherCameras = cameras.value.filter(({ uuid }) => uuid !== camera.uuid)
+    const otherCameras = cameras.value.filter(
+      ({ uuid }) => uuid !== camera.uuid,
+    )
     cameras.value = [camera, ...otherCameras]
   }
 
   watchEffect(() => {
     if (sizes.aspectRatio.value) {
-      cameras.value.forEach((camera: Camera) => {
-        if (camera instanceof PerspectiveCamera)
-          camera.aspect = sizes.aspectRatio.value
+      cameras.value.forEach((camera: Camera & { manual?: boolean }) => {
+        // NOTE: Don't mess with the camera if it belongs to the user.
+        // https://github.com/pmndrs/react-three-fiber/blob/0ef66a1d23bf16ecd457dde92b0517ceec9861c5/packages/fiber/src/core/utils.ts#L457
+        //
+        // To set camera as "manual":
+        // const myCamera = new PerspectiveCamera(); // or OrthographicCamera
+        // (myCamera as any).manual = true
+        if (!camera.manual) {
+          if (camera instanceof PerspectiveCamera)
+            camera.aspect = sizes.aspectRatio.value
 
-        if (camera instanceof PerspectiveCamera || camera instanceof OrthographicCamera)
-          camera.updateProjectionMatrix()
+          if (
+            camera instanceof PerspectiveCamera
+            || camera instanceof OrthographicCamera
+          )
+            camera.updateProjectionMatrix()
+        }
       })
     }
   })
