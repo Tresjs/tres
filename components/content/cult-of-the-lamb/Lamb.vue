97f82554e62cfe7eb6cd49e32d0c305f4ceb1dd5
Alvaro Saburido <alvaro.saburido@gmail.com>
2024-02-05 08:30:29 +0100

    feat: cult of the lamb bloom example (#92)

---
+++ components/content/cult-of-the-lamb/Lamb.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { LoopOnce } from 'three'
+
+const { nodes, animations } = await useGLTF('/models/cult-of-the-lamb/Lamb.glb', { draco: true })
+console.log(animations)
+const lamb = nodes['rig']
+
+const { actions, mixer } = useAnimations(animations, lamb)
+
+const currentAction = ref(actions['rigAction'])
+
+currentAction.value.clampWhenFinished = true
+currentAction.value.loop = LoopOnce
+currentAction.value.play()
+</script>
+
+<template>
+  <Levioso>
+    <primitive :object="lamb" />
+  </Levioso>
+</template>
---
+++ content/experiments/cult-of-the-lamb.md
@@ -0,0 +1,13 @@
+---
+thumbnail: /cult-of-the-lamb.png
+title: Cult of the lamb 🐑
+slug: cult-of-the-lamb
+author: alvarosabu
+status: published
+description: Best video game ever scene to showcase Postprocessing effects
+tags: ['fog', 'backdrop', 'unrealbloom', useGLTF]
+---
+
+::cult-of-the-lamb
+::
+
