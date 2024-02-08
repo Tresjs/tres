8b6cb67b686bf242aa6e96c3b68af535ddb944d1
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2024-02-08 15:34:54 +0000

    feat(app): Add RoundedBox component

---
+++ playground/src/components/TemplatePage.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { TresCanvas } from '@tresjs/core'
+import { OrbitControls } from '@tresjs/cientos'
+</script>
+
+<template>
+  <TresCanvas
+    window-size
+    clear-color="#111"
+  >
+    <TresPerspectiveCamera
+      :position="[0, 0, 3]"
+      :fov="45"
+      :aspect="1"
+      :near="0.1"
+      :far="1000"
+    />
+    <OrbitControls />
+    <TresMesh>
+      <TresBoxGeometry :args="[1, 1, 1]" />
+      <TresMeshBasicMaterial :color="0x00ff00" />
+    </TresMesh>
+    <TresDirectionalLight
+      :position="[0, 2, 4]"
+      :intensity="2"
+    />
+    <TresAmbientLight />
+  </TresCanvas>
+</template>
