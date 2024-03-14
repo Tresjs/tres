1824cbbacce0242a09f4c405437b4c51a77a1f0f
Damien Montastier <33467958+damienmontastier@users.noreply.github.com>
2024-03-14 16:30:24 +0100

    feat(cientos/positional-audio): start writing documentation

--- docs/.vitepress/theme/components/PositionalAudioDemo.vue
+++ docs/.vitepress/theme/components/PositionalAudioDemo.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core'
-import { OrbitControls } from '@tresjs/cientos'
+import { OrbitControls, PositionalAudio, Box } from '@tresjs/cientos'
+
+const gl = {
+  clearColor: '#82DBC5',
+  shadows: true,
+  alpha: false,
+}
 </script>
 
 <template>
-  <TresCanvas clear-color="#F78B3D">
-    <TresPerspectiveCamera :position="[3, 2, 5]" />
+  <TresCanvas v-bind="gl">
+    <TresPerspectiveCamera :position="[0, 0.5, 5]" />
     <OrbitControls />
 
-    <TresAmbientLight :intensity="1" />
+    <Box :args="[1, 1, 1]">
+      <TresMeshNormalMaterial />
+
+      <Suspense>
+        <PositionalAudio :ready="ready" ref="positionalAudioRef" :innerAngle="220" :outerAngle="220" :outerGain=".2"
+          :distance="2" helper url="/positional-audio/sound1.mp3" />
+      </Suspense>
+    </Box>
+
+    <Box :args="[4, 2, 0.1]" :position="[0, 0, -1]">
+      <TresMeshBasicMaterial color="#ff0000" transparent :opacity="0.5" />
+    </Box>
+
+    <TresGridHelper :position="[0, -.01, 0]" :args="[10, 10]" />
   </TresCanvas>
 </template>
