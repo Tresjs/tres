199c654a5f6df81d974588d3a1d4ec2e4c3a10ca
alvarosabu <alvaro.saburido@gmail.com>
2024-03-22 10:49:43 +0100

    docs: new event docs

--- playground/src/pages/raycaster/TheEvents.vue
+++ playground/src/pages/raycaster/TheEvents.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { TresCanvas } from '@tresjs/core'
+import { ThreeEvent, TresCanvas } from '@tresjs/core'
 import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three'
-
+import { TresLeches, useControls } from '@tresjs/leches';
 import { OrbitControls } from '@tresjs/cientos'
+import '@tresjs/leches/styles'
+import { func } from 'three/examples/jsm/nodes/Nodes.js';
 
 const gl = {
   clearColor: '#202020',
@@ -13,64 +15,80 @@ const gl = {
   toneMapping: NoToneMapping,
 }
 
-function onClick(ev) {
-  if (ev) {
-    ev.object.material.color.set('#008080')
-  }
+const { stopPropagation } = useControls({
+  stopPropagation: false,
+})
+
+function onClick(ev: ThreeEvent<MouseEvent>) {
+  console.log('click', ev)
+  if (stopPropagation.value) ev.stopPropagation()
+  ev.object.material.color.set('#008080')
 }
 
-function onPointerEnter(ev) {
-  if (ev) {
-    ev.object.material.color.set('#CCFF03')
-  }
+function onDoubleClick(ev: ThreeEvent<MouseEvent>) {
+  console.log('double-click', ev)
+  if (stopPropagation.value) ev.stopPropagation()
+  ev.object.material.color.set('#FFD700')
 }
 
-function onPointerLeave(ev) {
-  if (ev) {
+function onPointerEnter(ev: ThreeEvent<MouseEvent>) {
+  if (stopPropagation.value) ev.stopPropagation()
+  ev.object.material.color.set('#CCFF03')
+}
+
+function onPointerLeave(ev: ThreeEvent<MouseEvent>) {
+  if (stopPropagation.value) ev.stopPropagation()
     /*  ev.object.material.color.set('#efefef') */
-  }
 }
 
-function onPointerMove(ev) {
-  if (ev) {
-    console.log(ev)
-  }
+function onPointerMove(ev: ThreeEvent<MouseEvent>) {
+  if (stopPropagation.value) ev.stopPropagation()
 }
 
-const visible = ref(true)
+function onContextMenu(ev: ThreeEvent<MouseEvent>) {
+  console.log('context-menu', ev)
+  if (stopPropagation.value) ev.stopPropagation()
+  ev.object.material.color.set('#FF4500')
+}
+
+function onPointerMissed(ev: ThreeEvent<MouseEvent>) {
+  console.log('pointer-missed', ev)
+  if (stopPropagation.value) ev.stopPropagation()
+}
+
 </script>
 
 <template>
-  <button @click="visible = !visible" />
-  <div v-if="visible">
-    <TresCanvas
-      window-size
-      v-bind="gl"
-    >
-      <TresPerspectiveCamera
-        :position="[11, 11, 11]"
-        :look-at="[0, 0, 0]"
-      />
-      <OrbitControls />
-
-      <template v-for="x in [-2.5, 0, 2.5]">
-        <template v-for="y in [-2.5, 0, 2.5]">
-          <TresMesh
-            v-for="z in [-2.5, 0, 2.5]"
-            :key="`${[x, y, z]}`"
-            :position="[x, y, z]"
-            @click="onClick"
-            @pointer-enter="onPointerEnter"
-            @pointer-leave="onPointerLeave"
-            @pointer-move="onPointerMove"
-          >
-            <TresBoxGeometry :args="[1, 1, 1]" />
-            <TresMeshToonMaterial color="#efefef" />
-          </TresMesh>
-        </template>
+  <TresLeches />
+  <TresCanvas
+    window-size
+    v-bind="gl"
+  >
+    <TresPerspectiveCamera
+      :position="[11, 11, 11]"
+      :look-at="[0, 0, 0]"
+    />
+    <OrbitControls />
+    <template v-for="x in [-2.5, 0, 2.5]">
+      <template v-for="y in [-2.5, 0, 2.5]">
+        <TresMesh
+          v-for="z in [-2.5, 0, 2.5]"
+          :key="`${[x, y, z]}`"
+          :position="[x, y, z]"
+          @click="onClick"
+          @double-click="onDoubleClick"
+          @pointer-enter="onPointerEnter"
+          @pointer-leave="onPointerLeave"
+          @pointer-move="onPointerMove"
+          @context-menu="onContextMenu"
+          @pointer-missed="onPointerMissed"
+        >
+          <TresBoxGeometry :args="[1, 1, 1]" />
+          <TresMeshToonMaterial color="#efefef" />
+        </TresMesh>
       </template>
-      <TresDirectionalLight :intensity="1" />
-      <TresAmbientLight :intensity="1" />
-    </TresCanvas>
-  </div>
+    </template>
+    <TresDirectionalLight :intensity="1" />
+    <TresAmbientLight :intensity="1" />
+  </TresCanvas>
 </template>
