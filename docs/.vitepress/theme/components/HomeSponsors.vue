a3aa14a1f5e294568c83235edf127f167964b75e
alvarosabu <alvaro.saburido@gmail.com>
2024-04-19 15:04:00 +0200

    docs: added Daniel

--- docs/.vitepress/theme/components/HomeSponsors.vue
+++ docs/.vitepress/theme/components/HomeSponsors.vue
@@ -90,7 +90,17 @@ import { VPHomeSponsors } from 'vitepress/theme'
         target="_blank"
         rel="noreferrer"
       >
-        Nuxt <img class="w-5 h-5 rounded ml-2" src="https://avatars.githubusercontent.com/u/23360933?s=160&v=4" alt="" />
+        Daniel Roe <img class="w-5 h-5 rounded ml-2" src="https://avatars.githubusercontent.com/u/23360933?s=160&v=4" alt="" />
+      </a>
+    </li>
+    <li>
+      <a
+        class="sponsor"
+        href="https://github.com/sponsors/danielroe"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Nuxt <img class="w-5 h-5 rounded ml-2" src="https://avatars.githubusercontent.com/u/28706372?v=4" alt="" />
       </a>
     </li>
   </ul>
