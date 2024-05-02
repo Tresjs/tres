22875cc7b6c5fe68789336162adc73a0a4f3087f
Peter <petermoldovia@yahoo.ca>
2024-05-02 20:08:12 +0200

    test: test setting not calling

--- src/core/nodeOps.test.ts
+++ src/core/nodeOps.test.ts
@@ -301,35 +301,25 @@ describe('nodeOps', () => {
       expect(node.defines[allCapsUnderscoresKey]).equals(allCapsUnderscoresValue)
     })
 
-    it('sets "on*" callbacks on Object3D', () => {
+    it('replaces "on*" methods on Object3D', () => {
       // Issue: https://github.com/Tresjs/tres/issues/360
       const { createElement, patchProp } = nodeOps
       const object = createElement('TresObject3D', undefined, undefined, {})
 
-      const onAfterRender = vi.fn()
-      const onAfterShadow = vi.fn()
-      const onBeforeRender = vi.fn()
-      const onBeforeShadow = vi.fn()
+      const onAfterRender = () => {}
+      const onAfterShadow = () => {}
+      const onBeforeRender = () => {}
+      const onBeforeShadow = () => {}
 
       patchProp(object, 'onAfterRender', null, onAfterRender)
       patchProp(object, 'onAfterShadow', null, onAfterShadow)
       patchProp(object, 'onBeforeRender', null, onBeforeRender)
       patchProp(object, 'onBeforeShadow', null, onBeforeShadow)
 
-      expect(onAfterRender).not.toBeCalled()
-      expect(onAfterShadow).not.toBeCalled()
-      expect(onBeforeRender).not.toBeCalled()
-      expect(onBeforeShadow).not.toBeCalled()
-
-      object.onAfterRender()
-      object.onAfterShadow()
-      object.onBeforeRender()
-      object.onBeforeShadow()
-
-      expect(onAfterRender).toBeCalled()
-      expect(onAfterShadow).toBeCalled()
-      expect(onBeforeRender).toBeCalled()
-      expect(onBeforeShadow).toBeCalled()
+      expect(object.onAfterRender).toBe(onAfterRender)
+      expect(object.onAfterShadow).toBe(onAfterShadow)
+      expect(object.onBeforeRender).toBe(onBeforeRender)
+      expect(object.onBeforeShadow).toBe(onBeforeShadow)
     })
 
     it('calls object methods', () => {
