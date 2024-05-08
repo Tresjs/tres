7318359463e0fbb0bb84b6af5d15c957bd2f7218
Luckystriike <glenny@me.com>
2024-05-08 18:30:19 +0200

    add github btn

--- components/content/dancing-blob/index.vue
+++ components/content/dancing-blob/index.vue
@@ -1,4 +1,8 @@
 <template>
+  <button
+    class="gitBtn bg-gray-600 hover:bg-gray-700 opacity-40 transition-color shadow-lg hover:shadow-xl infline-flex w-12 h-12 justify-center items-center rounded-full absolute bottom-2 right-2">
+    <a href="https://github.com/luckystriike22/TresJsPlayground" target="_blank">Code</a>
+  </button>
   <TresCanvas :clear-color="'#0c1a30'">
     <TresPerspectiveCamera :position="[13, 0, 0]" />
     <OrbitControls />
@@ -9,3 +13,11 @@
 </template>
 
 <script lang="ts" setup></script>
+<style scoped>
+.gitBtn {
+  margin-bottom: 10px;
+  margin-right: 10px;
+  z-index: 10;
+  color: white;
+}
+</style>
