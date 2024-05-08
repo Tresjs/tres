117985bd8fe39cc359c3fd9cedfef641bb408f16
Luckystriike <glenny@me.com>
2024-05-08 16:33:39 +0200

    Add a dancing blob demo

---
+++ components/content/dancing-blob/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <TresCanvas :clear-color="'#0c1a30'">
+    <TresPerspectiveCamera :position="[13, 0, 0]" />
+    <OrbitControls />
+    <Suspense>
+      <MusicBlob />
+    </Suspense>
+    <TresDirectionalLight :position="[1, 1, 1]" />
+    <TresAmbientLight :intensity="1" />
+  </TresCanvas>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
---
+++ content/experiments/dancing-blob.md
@@ -0,0 +1,11 @@
+---
+thumbnail: /dancing-blob.png
+title: Dancing blob
+slug: dancing-blob
+author: Luckystriike
+status: published
+description: A sphere dancing to the sound of your microphone.
+tags: ['basic', 'TresJs', 'audio', 'shader']
+---
+
+<DancingBlob />
