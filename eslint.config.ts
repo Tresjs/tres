c006fd2c62d25edd9246d1102a664a09d9076519
alvarosabu <alvaro.saburido@gmail.com>
2024-05-16 17:09:27 +0200

    chore: fix my own lint

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,6 @@ import { tresLintConfig } from './src/index'
 export default tresLintConfig({
   files: ['**/*.vue'],
   rules: {
-    'vue/attribute-hyphenation': 'off' // To allow camelCase attributes in cases that require, like material-uniforms, shadowMapSizes, etc.
+    'vue/attribute-hyphenation': 'off', // To allow camelCase attributes in cases that require, like material-uniforms, shadowMapSizes, etc.
   },
 })
