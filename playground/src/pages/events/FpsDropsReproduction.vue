c1bce06d65119db79569c886a435909c9d994ba0
alvarosabu <alvaro.saburido@gmail.com>
2024-06-02 13:06:54 +0200

    fix: intersect only objects with events registered.

---
+++ playground/src/pages/events/FpsDropsReproduction.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+import { TresCanvas, useRenderLoop } from '@tresjs/core'
+import { Icosahedron, OrbitControls, StatsGl } from '@tresjs/cientos'
+import {
+  AgXToneMapping,
+  Color,
+  MeshPhysicalMaterial,
+  PCFSoftShadowMap,
+  SRGBColorSpace,
+} from 'three'
+
+const gl = {
+  shadows: true,
+  physicallyCorrectLights: true,
+  gammaFactor: 2.2,
+  gammaOutput: true,
+  outputColorSpace: SRGBColorSpace,
+  toneMapping: AgXToneMapping,
+  toneMappingExposure: 2.2,
+  shadowMapType: PCFSoftShadowMap,
+  powerPreference: 'high-performance',
+  antialias: true,
+}
+</script>
+
+<template>
+  <TresCanvas clear-color="#ccc" v-bind="gl" window-size preset="realistic">
+    <StatsGl />
+    <TresPerspectiveCamera :position="[0, 0, 15]" :args="[45, 1, 0.1, 1000]" />
+    <OrbitControls />
+    <TresDirectionalLight
+      :intensity="0.4"
+      :position="[30, 20, 10]"
+      :color="0xFFFFFF"
+      :bias="-0.0001"
+    />
+    <TresDirectionalLight
+      :intensity="0.4"
+      :position="[-30, -20, 40]"
+      :color="0xFFFFFF"
+      :bias="-0.0001"
+      castShadow
+    />
+    <TresHemisphereLight :intensity="1" :color="0xFFFFFF" :ground-color="0xFFFFFF" />
+    >
+    <Icosahedron :args="[4, 150]" :position="[0, 0, 0]" :rotation="[Math.PI, 0, 0]">
+      <TresMeshNormalMaterial wireframe />
+    </Icosahedron>
+  </TresCanvas>
+</template>
