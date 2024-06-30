9af94fda12e36b8d957c18bb6e665ccbd75a2a75
Peter <petermoldovia@yahoo.ca>
2024-07-01 00:42:50 +0200

    fix(primitive): add playground

--- playground/src/router/routes/issues.ts
+++ playground/src/router/routes/issues.ts
@@ -1,5 +1,10 @@
 export const issuesRoutes = [
   {
+    path: '/issues/701',
+    name: '#701: primitive :object',
+    component: () => import('../../pages/issues/701/index.vue'),
+  },
+  {
     path: '/issues/717vIf',
     name: '#717: v-if',
     component: () => import('../../pages/issues/717/index.vue'),
