5f2417af4bd1963170c1192961dfb3a111a42784
Francesco Michelini <hello@francescomichelini.com>
2024-07-19 22:19:33 +0200

    Lint

--- playground/src/pages/webgpu/WebGPURenderer.vue
+++ playground/src/pages/webgpu/WebGPURenderer.vue
@@ -1,44 +1,43 @@
 <script setup lang="ts">
 import {
-	BasicShadowMap,
-	MeshToonMaterial,
-	NoToneMapping,
-	SRGBColorSpace,
+  BasicShadowMap,
+  NoToneMapping,
+  SRGBColorSpace,
 } from 'three'
 import { TresCanvas, useRenderLoop } from '@tresjs/core'
 import { OrbitControls } from '@tresjs/cientos'
-import { onMounted, nextTick } from 'vue'
+import { nextTick, onMounted } from 'vue'
 
 const state = reactive({
-	clearColor: '#201919',
-	shadows: false,
-	alpha: false,
+  clearColor: '#201919',
+  shadows: false,
+  alpha: false,
 
-	shadowMapType: BasicShadowMap,
-	outputColorSpace: SRGBColorSpace,
-	toneMapping: NoToneMapping,
+  shadowMapType: BasicShadowMap,
+  outputColorSpace: SRGBColorSpace,
+  toneMapping: NoToneMapping,
 })
 
 const { onLoop } = useRenderLoop()
 
 onMounted(async () => {
-	await nextTick()
+  await nextTick()
 
-	onLoop(({ elapsed }) => {})
+  onLoop(({ elapsed }) => {})
 })
 </script>
 
 <template>
-	<TresCanvas v-bind="state">
-		<TresPerspectiveCamera :position="[0, 1, -3]" :look-at="[0, 0, 0]" />
+  <TresCanvas v-bind="state">
+    <TresPerspectiveCamera :position="[0, 1, -3]" :look-at="[0, 0, 0]" />
 
-		<OrbitControls />
+    <OrbitControls />
 
-		<TresAmbientLight :intensity="0.5" />
+    <TresAmbientLight :intensity="0.5" />
 
-		<TresMesh>
-			<TresBoxGeometry />
-			<TresMeshStandardMaterial color="red" />
-		</TresMesh>
-	</TresCanvas>
+    <TresMesh>
+      <TresBoxGeometry />
+      <TresMeshStandardMaterial color="red" />
+    </TresMesh>
+  </TresCanvas>
 </template>
