46f6118b543413964484bdc1cb39232524a61ee9
Francesco Michelini <hello@francescomichelini.com>
2024-07-19 22:16:10 +0200

    Add WebGPU section

---
+++ playground/src/pages/webgpu/WebGPURenderer.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import {
+	BasicShadowMap,
+	MeshToonMaterial,
+	NoToneMapping,
+	SRGBColorSpace,
+} from 'three'
+import { TresCanvas, useRenderLoop } from '@tresjs/core'
+import { OrbitControls } from '@tresjs/cientos'
+import { onMounted, nextTick } from 'vue'
+
+const state = reactive({
+	clearColor: '#201919',
+	shadows: false,
+	alpha: false,
+
+	shadowMapType: BasicShadowMap,
+	outputColorSpace: SRGBColorSpace,
+	toneMapping: NoToneMapping,
+})
+
+const { onLoop } = useRenderLoop()
+
+onMounted(async () => {
+	await nextTick()
+
+	onLoop(({ elapsed }) => {})
+})
+</script>
+
+<template>
+	<TresCanvas v-bind="state">
+		<TresPerspectiveCamera :position="[0, 1, -3]" :look-at="[0, 0, 0]" />
+
+		<OrbitControls />
+
+		<TresAmbientLight :intensity="0.5" />
+
+		<TresMesh>
+			<TresBoxGeometry />
+			<TresMeshStandardMaterial color="red" />
+		</TresMesh>
+	</TresCanvas>
+</template>
