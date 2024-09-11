7deb26f01eecc7b48a626f78881660a1071f57a5
Jaime A Torrealba C <63722373+JaimeTorrealba@users.noreply.github.com>
2024-09-11 19:45:25 +0100

    feat(app): 111 Add gravity as a props (#114)
    * feat(app): 111 Add gravity as a props
    
    * feat: handle gravity as Vector, array and object format + lint issues
    
    * feat: accept VectorCoordinates for gravity
    
    ---------
    
    Co-authored-by: alvarosabu <alvaro.saburido@gmail.com>

--- playground/src/router/routes/basics.ts
+++ playground/src/router/routes/basics.ts
@@ -4,4 +4,9 @@ export const basicsRoutes = [
     name: 'Rigid Body',
     component: () => import('../../pages/basics/RigidBodyDemo.vue'),
   },
+  {
+    path: '/basics/gravity',
+    name: 'Gravity',
+    component: () => import('../../pages/basics/GravityDemo.vue'),
+  },
 ]
--- src/components/Physics.vue
+++ src/components/Physics.vue
@@ -1,18 +1,48 @@
 <script setup lang="ts">
 import { useLoop } from '@tresjs/core'
+import { Vector3 } from 'three'
 
+import { watch } from 'vue'
+import type { TresVector3, VectorCoordinates } from '@tresjs/core'
 import { useRapierContextProvider } from '../composables/useRapier'
+// import type { PhysicsProps } from '../types/physics'
+import { GRAVITY } from '../constants/physics'
 import Debug from './Debug.vue'
-import type { PhysicsProps } from '../types/physics'
 
-withDefaults(
+export interface PhysicsProps {
+  debug: boolean
+  gravity: TresVector3 | VectorCoordinates
+}
+
+const props = withDefaults(
   defineProps<Partial<PhysicsProps>>(),
   {
+    gravity: () => new Vector3(GRAVITY.x, GRAVITY.y, GRAVITY.z),
     debug: false,
   },
 )
 
 const { world } = await useRapierContextProvider()
+
+const setGravity = (gravity: TresVector3) => {
+  // If gravity is somethign like [0, -9.8, 0]
+  if (Array.isArray(gravity)) {
+    world.gravity.x = gravity[0]
+    world.gravity.y = gravity[1]
+    world.gravity.z = gravity[2]
+  }
+  else {
+    const coordinates = gravity as VectorCoordinates
+    world.gravity.x = coordinates.x
+    world.gravity.y = coordinates.y
+    world.gravity.z = coordinates.z
+  }
+}
+
+watch(() => props.gravity, (gravity) => {
+  setGravity(gravity)
+}, { immediate: true })
+
 const { onBeforeRender } = useLoop()
 
 onBeforeRender(() => {
