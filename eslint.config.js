dfeecc7f134defb7285ec1eed80c7f51e155cb4b
Alvaro Saburido <alvaro.saburido@gmail.com>
2024-10-04 16:36:51 +0200

    chore: clean up config (#498)

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,4 @@
 import { tresLintConfig } from '@tresjs/eslint-config'
 
 export default tresLintConfig({
-  ignores: ['dist', 'node_modules', 'public', '.github', 'docs/blog'],
-}, {
-  rules: {
-    'style/max-statements-per-line': 'off',
-    'jsdoc/check-alignment': 'off',
-  },
 })
