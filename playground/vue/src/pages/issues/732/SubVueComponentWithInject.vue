35125cecda55333bac542ed473bcddd2f7925c2b
eriklumme1 <141633336+eriklumme1@users.noreply.github.com>
2024-10-12 17:15:03 +0300

    fix: 849 support Symbol keys in provide/inject (#850)

--- playground/vue/src/pages/issues/732/SubVueComponentWithInject.vue
+++ playground/vue/src/pages/issues/732/SubVueComponentWithInject.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import { inject, watchEffect } from 'vue'
+import { injectSymbol } from './constants'
 
 const awiwi = inject('awiwi')
 const bululu = inject('bululu')
+const symbolInjection = inject(injectSymbol)
 // eslint-disable-next-line no-console
 watchEffect(() => console.log('vue:awiwi', awiwi?.a))
 // eslint-disable-next-line no-console
 watchEffect(() => console.log('vue:bululu', bululu?.value))
+// eslint-disable-next-line no-console
+watchEffect(() => console.log('vue:symbolInjection', symbolInjection?.value))
 // eslint-disable-next-line no-console
 watchEffect(() => console.log('vue:v-route', inject('v-route')))
 // eslint-disable-next-line no-console
@@ -16,5 +20,6 @@ watchEffect(() => console.log('vue:useTres', inject('useTres')))
 <template>
   <p>awiwi: {{ awiwi }}</p>
   <p>bululu: {{ bululu }}</p>
+  <p>symbolInjection: {{ symbolInjection }}</p>
   <p>v-route: {{ inject('v-route') }}</p>
 </template>
