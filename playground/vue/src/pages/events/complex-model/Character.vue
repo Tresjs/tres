92862212aa503f546aea5415d69a7ecab4d91e58
alvarosabu <alvaro.saburido@gmail.com>
2025-02-22 11:46:26 +0100

    chore(playground): add complex model and group event handling examples
    - Add new pages for complex model and group event interactions
    - Implement event handlers for click, pointer-enter, and pointer-leave events
    - Include Cyber Samurai and Artificer 3D models with event listeners
    - Add new routes for complex model and groups event demonstrations

---
+++ playground/vue/src/pages/events/complex-model/Character.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useGLTF } from '@tresjs/cientos'
+
+const { nodes } = await useGLTF('/models/Artificer.glb', { draco: true })
+console.log(nodes)
+const model = nodes.Engineer_Rig
+
+model.position.set(-4, 0, 0)
+
+const handleClick = (e: PointerEvent) => {
+  console.log('clicked', e)
+}
+
+const handlePointerEnter = (e: PointerEvent) => {
+  console.log('pointer-enter', e)
+}
+
+const handlePointerLeave = (e: PointerEvent) => {
+  console.log('pointer-leave', e)
+}
+</script>
+
+<template>
+  <primitive :object="model" @click="handleClick" @pointer-enter="handlePointerEnter" @pointer-leave="handlePointerLeave" />
+</template>
---
+++ playground/vue/src/pages/events/complex-model/ComplexModel.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useGLTF } from '@tresjs/cientos'
+
+const { nodes } = await useGLTF('/models/cyber_samurai/scene.gltf', { draco: true })
+console.log(nodes)
+const model = nodes.Sketchfab_model
+
+const handleClick = (e: PointerEvent) => {
+  console.log('clicked', e)
+}
+
+const handlePointerEnter = (e: PointerEvent) => {
+  console.log('pointer-enter', e)
+}
+
+const handlePointerLeave = (e: PointerEvent) => {
+  console.log('pointer-leave', e)
+}
+</script>
+
+<template>
+  <primitive :object="model" @click="handleClick" @pointer-enter="handlePointerEnter" @pointer-leave="handlePointerLeave" />
+</template>
