041b697bf77ad7b7f70b7d3f2bdeed99c5149abb
Alvaro Saburido <alvaro.saburido@gmail.com>
2025-05-13 11:24:47 +0200

    feat!: 986 remove usetexture (#1008)
    * feat!: remove useTexture composable and update documentation
    
    BREAKING CHANGE: `useTexture` composable has been refactored and moved to `@tresjs/cientos` package
    
    - Deleted the `useTexture` composable and its associated files as it has been refactored and moved to the `@tresjs/cientos` package.
    - Updated documentation to reflect the changes, including a warning about the deprecation and a link to the new `cientos` documentation for `useTexture`.
    - Adjusted examples in the cookbook to utilize the new `useTexture` from `@tresjs/cientos`, ensuring users are directed to the correct implementation.
    
    * fix: clean up texture loading examples in documentation
    
    - Removed unnecessary line breaks and improved formatting in the `load-textures.md` documentation.
    - Streamlined the example code for `TresMeshStandardMaterial` to enhance readability and maintain consistency with coding standards.
    
    * docs: update composables documentation to reflect removal of useTexture
    
    - Removed deprecated `useTexture` section from the documentation, indicating its refactor to the `@tresjs/cientos` package.
    - Added a warning about the deprecation and provided a link to the new `cientos` documentation for user guidance.
    - Ensured that the remaining documentation is clear and concise, maintaining consistency with the latest changes in the codebase.

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,5 +1,4 @@
 import UseLoader from './useLoader/component.vue'
-import UseTexture from './useTexture/component.vue'
 
 export * from './useCamera/'
 export * from './useGraph'
@@ -8,7 +7,7 @@ export * from './useLoop'
 export * from './useRaycaster'
 export * from './useRenderer/useRendererManager'
 export * from './useRenderLoop'
-export * from './useTexture'
+
 export * from './useTresContextProvider'
 export * from './useTresEventManager'
-export { UseLoader, UseTexture }
+export { UseLoader }
