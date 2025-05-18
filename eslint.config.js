bd799fb46c7d37a3f1bab15f1ca5664c812c76ad
Alvaro Saburido <alvaro.saburido@gmail.com>
2025-05-18 16:34:14 +0200

    chore(deps): update dependencies and devDependencies (#162)
    * chore(deps): update dependencies and devDependencies
    
    - Updated `@dimforge/rapier3d-compat` from `^0.14.0` to `^0.17.0`
    - Updated `@release-it/conventional-changelog` from `^9.0.3` to `^10.0.1`
    - Updated `@tresjs/core` from `^4.3.1` to `^4.3.5`
    - Updated `@types/node` from `^22.10.2` to `^22.15.18`
    - Updated `@types/three` from `^0.170.0` to `^0.176.0`
    - Updated `@vitejs/plugin-vue` from `^5.2.1` to `^5.2.4`
    - Updated `eslint` from `^9.16.0` to `^9.27.0`
    - Updated `eslint-plugin-vue` from `^9.32.0` to `^10.1.0`
    - Updated `pathe` from `^1.1.2` to `^2.0.3`
    - Updated `release-it` from `^17.10.0` to `^19.0.2`
    - Updated `three` from `^0.171.0` to `^0.176.0`
    - Updated `typescript` from `^5.7.2` to `^5.8.3`
    - Updated `unocss` from `^0.65.1` to `^66.1.2`
    - Updated `vite` from `^6.0.3` to `^6.3.5`
    - Updated `vite-plugin-banner` from `^0.8.0` to `^0.8.1`
    - Updated `vite-plugin-dts` from `^4.3.0` to `^4.5.4`
    - Updated `vitepress` from `1.3.4` to `1.6.3`
    - Updated `vue` from `^3.5.13` to `^3.5.14`
    - Updated `vue-tsc` from `^2.1.10` to `^2.2.10`
    
    * ci: update ubuntu version

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ export default tresLintConfig({
 }, {
   rules: {
     'style/max-statements-per-line': 'off',
-    'jsdoc/check-alignment': 'off',
+    // TODO: temporary fix for Error while loading rule 'jsonc/no-useless-escape': Cannot read properties of undefined (reading 'allowRegexCharacters')
+    'jsonc/no-useless-escape': 'off',
   },
 })
