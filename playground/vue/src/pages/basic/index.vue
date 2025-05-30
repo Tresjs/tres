477c524adc25c438efdd157d615eff3c0afdf013
alvarosabu <alvaro.saburido@gmail.com>
2025-05-30 16:46:01 +0200

    chore(playground): add controls to basic playground

--- playground/vue/src/pages/basic/index.vue
+++ playground/vue/src/pages/basic/index.vue
@@ -1,10 +1,53 @@
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core'
+import { ACESFilmicToneMapping, AgXToneMapping, BasicShadowMap, CineonToneMapping, LinearToneMapping, NeutralToneMapping, NoToneMapping, OrthographicCamera, PCFShadowMap, PCFSoftShadowMap, PerspectiveCamera, ReinhardToneMapping, VSMShadowMap } from 'three'
 import { OrbitControls } from '@tresjs/cientos'
+import { TresLeches, useControls } from '@tresjs/leches'
+import '@tresjs/leches/styles'
+
+const { clearColor, clearAlpha, toneMapping, shadows, shadowMapType } = useControls({
+  clearColor: '#82DBC5',
+  clearAlpha: {
+    value: 1,
+    min: 0,
+    max: 1,
+    step: 0.01,
+    label: 'Clear Alpha',
+  },
+  toneMapping: {
+    value: ACESFilmicToneMapping,
+    options: [
+      { text: 'No Tone Mapping', value: NoToneMapping },
+      { text: 'Linear', value: LinearToneMapping },
+      { text: 'Reinhard', value: ReinhardToneMapping },
+      { text: 'Cineon', value: CineonToneMapping },
+      { text: 'ACES Filmic', value: ACESFilmicToneMapping },
+      { text: 'AgX', value: AgXToneMapping }, // New in Three.js r155
+      { text: 'Neutral', value: NeutralToneMapping },
+    ],
+  },
+  shadows: true,
+  shadowMapType: {
+    value: PCFSoftShadowMap,
+    options: [
+      { text: 'Basic', value: BasicShadowMap },
+      { text: 'PCF', value: PCFShadowMap },
+      { text: 'PCF Soft', value: PCFSoftShadowMap },
+      { text: 'VSM', value: VSMShadowMap },
+    ],
+  },
+})
 </script>
 
 <template>
-  <TresCanvas clear-color="#82DBC5">
+  <TresLeches />
+  <TresCanvas
+    :clear-color="clearColor"
+    :clear-alpha="clearAlpha"
+    :tone-mapping="toneMapping"
+    :shadows="shadows"
+    :shadow-map-type="shadowMapType"
+  >
     <TresPerspectiveCamera :position="[8, 8, 8]" />
     <OrbitControls />
     <TresGridHelper />
