6ae62c308062afb59f0fa80eb0432f3b6fa28cc4
colinscz <19342760+colinscz@users.noreply.github.com>
2025-06-15 18:43:39 +0200

    feat: handle tres-canvas kebab case and temporarily disable eslint rule for pascal case

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ const plugin: TresPlugin = {
   install(app: App) {
     // Register core components
     app.component('TresCanvas', TresCanvas)
+    app.component('tres-canvas', TresCanvas)
   },
 }
 
