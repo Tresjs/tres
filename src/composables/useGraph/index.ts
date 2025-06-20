bd4a45e295621558f52f44acef640748d11c6601
Alvaro Saburido <alvaro.saburido@gmail.com>
2025-06-20 18:52:34 +0200

    chore: update dependencies to latest versions (#1041)
    - Updated `@types/three` from `^0.173.0` to `^0.177.0`.
    - Updated `three` from `^0.173.0` to `^0.177.0`.
    - Updated `vite-plugin-dts` from `4.5.0` to `4.5.4`.
    - Updated `vue` from `3.5.13` to `3.5.17`.
    - Updated related dependencies in `pnpm-lock.yaml` to reflect the changes in `package.json`.

--- src/composables/useGraph/index.ts
+++ src/composables/useGraph/index.ts
@@ -1,9 +1,8 @@
-import type { Object3D } from 'three'
 import type { MaybeRef } from '@vueuse/core'
 import { buildGraph } from '../../utils/graph'
 import { computed, toValue } from 'vue'
 import type { TresObject } from '../../types'
 
-export function useGraph(object: MaybeRef<Object3D | TresObject>) {
-  return computed(() => buildGraph(toValue(object) as Object3D))
+export function useGraph(object: MaybeRef<TresObject>) {
+  return computed(() => buildGraph(toValue(object)))
 }
