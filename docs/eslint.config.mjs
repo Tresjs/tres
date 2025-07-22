62f154a7caa2566255a86b55b70282a7ba2f6eb4
alvarosabu <alvaro.saburido@gmail.com>
2025-07-22 16:17:42 +0200

    chore: turn off annoying stylistic rule from nuxt eslint

--- docs/eslint.config.mjs
+++ docs/eslint.config.mjs
@@ -3,4 +3,9 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   // Your custom configs here
+  {
+    rules: {
+      '@stylistic/comma-dangle': ['off', 'always-multiline'],
+    },
+  },
 )
