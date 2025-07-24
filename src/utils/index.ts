67d2a3723941765b989f2d13024b61a630c47c56
Tino Koch <17991193+Tinoooo@users.noreply.github.com>
2025-07-25 00:06:08 +0200

    refactor: reorganize HTML tag handling and utility functions
    - Moved the `HTML_TAGS` constant and `isHTMLTag` function from `src/utils/index.ts` to `src/utils/is.ts` for better modularity.
    - Removed the `makeMap` function from `index.ts` and imported it in `is.ts`, enhancing code clarity and separation of concerns.
    - Cleaned up unused code in `index.ts` to streamline the utility functions.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -24,19 +24,6 @@ export const merge = (target: any, source: any) => {
   return target
 }
 
-const HTML_TAGS
-  = 'html,body,base,head,link,meta,style,title,address,article,aside,footer,'
-    + 'header,hgroup,h1,h2,h3,h4,h5,h6,nav,section,div,dd,dl,dt,figcaption,'
-    + 'figure,picture,hr,img,li,main,ol,p,pre,ul,a,b,abbr,bdi,bdo,br,cite,code,'
-    + 'data,dfn,em,i,kbd,mark,q,rp,rt,ruby,s,samp,small,span,strong,sub,sup,'
-    + 'time,u,var,wbr,area,audio,map,track,video,embed,object,param,source,'
-    + 'canvas,script,noscript,del,ins,caption,col,colgroup,table,thead,tbody,td,'
-    + 'th,tr,button,datalist,fieldset,form,input,label,legend,meter,optgroup,'
-    + 'option,output,progress,select,textarea,details,dialog,menu,'
-    + 'summary,template,blockquote,iframe,tfoot'
-
-export const isHTMLTag = /* #__PURE__ */ makeMap(HTML_TAGS)
-
 export function isDOMElement(obj: any): obj is HTMLElement {
   return obj && obj.nodeType === 1
 }
@@ -49,15 +36,6 @@ export function kebabToCamel(str: string) {
 const hyphenateRE = /\B([A-Z])/g
 export function hyphenate(str: string) {
   return str.replace(hyphenateRE, '-$1').toLowerCase()
-}
-
-export function makeMap(str: string, expectsLowerCase?: boolean): (key: string) => boolean {
-  const map: Record<string, boolean> = Object.create(null)
-  const list: Array<string> = str.split(',')
-  for (let i = 0; i < list.length; i++) {
-    map[list[i]] = true
-  }
-  return expectsLowerCase ? val => !!map[val.toLowerCase()] : val => !!map[val]
 }
 
 export const uniqueBy = <T, K>(array: T[], iteratee: (value: T) => K): T[] => {
--- src/utils/is.ts
+++ src/utils/is.ts
@@ -1,6 +1,7 @@
 import type { TresCamera, TresInstance, TresObject, TresPrimitive } from 'src/types'
 import type { BufferGeometry, Color, ColorRepresentation, Fog, Light, Material, Object3D, OrthographicCamera, PerspectiveCamera, Scene } from 'three'
 import { Layers } from 'three'
+import { makeMap } from './makeMap'
 
 /**
  * Type guard to check if a value is undefined
@@ -362,3 +363,16 @@ export function isTresPrimitive(value: unknown): value is TresPrimitive {
 export function isTresInstance(value: unknown): value is TresInstance {
   return isTresObject(value) && '__tres' in value
 }
+
+const HTML_TAGS
+  = 'html,body,base,head,link,meta,style,title,address,article,aside,footer,'
+    + 'header,hgroup,h1,h2,h3,h4,h5,h6,nav,section,div,dd,dl,dt,figcaption,'
+    + 'figure,picture,hr,img,li,main,ol,p,pre,ul,a,b,abbr,bdi,bdo,br,cite,code,'
+    + 'data,dfn,em,i,kbd,mark,q,rp,rt,ruby,s,samp,small,span,strong,sub,sup,'
+    + 'time,u,var,wbr,area,audio,map,track,video,embed,object,param,source,'
+    + 'canvas,script,noscript,del,ins,caption,col,colgroup,table,thead,tbody,td,'
+    + 'th,tr,button,datalist,fieldset,form,input,label,legend,meter,optgroup,'
+    + 'option,output,progress,select,textarea,details,dialog,menu,'
+    + 'summary,template,blockquote,iframe,tfoot'
+
+export const isHTMLTag = /* #__PURE__ */ makeMap(HTML_TAGS)
