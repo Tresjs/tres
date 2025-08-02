a4d23d0ee357eae3abf7b8e40cc724d646864a90
Tino Koch <17991193+Tinoooo@users.noreply.github.com>
2025-08-02 17:46:45 +0200

    chore: made use of isMesh in docs

--- docs/app/components/examples/web-gpu/HologramCube.vue
+++ docs/app/components/examples/web-gpu/HologramCube.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { isMesh } from '@tresjs/core'
 import { useGLTF } from '@tresjs/cientos'
 import { add, cameraProjectionMatrix, cameraViewMatrix, color, Fn, hash, mix, normalView, positionWorld, sin, timerGlobal, uniform, varying, vec3, vec4 } from 'three/tsl'
 import type { Mesh, Object3D } from 'three/webgpu'
@@ -46,8 +47,6 @@ material.colorNode = Fn(() => {
 })()
 
 watch(model, (newModel) => {
-  const isMesh = (child: Object3D): child is Mesh => 'isMesh' in child && !!(child.isMesh)
-
   newModel?.traverse((child) => {
     if (isMesh(child)) {
       child.material = material
