0198eda9d07a0b378a32081cdd6a14a19fce85b6
userquin <userquin@gmail.com>
2025-09-24 17:13:13 +0200

    chore: cleanup vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@
 
 import vue from '@vitejs/plugin-vue'
 
-// import copy from 'rollup-plugin-copy'
 import { defineConfig } from 'vite'
-// import dts from 'vite-plugin-dts'
 
 /* import analyze from 'rollup-plugin-analyzer' */
 
@@ -31,51 +29,12 @@ export default defineConfig({
         },
       },
     }),
-    /* dts({
-      insertTypesEntry: true,
-    }), */
-    /* banner({
-      content: `/!**\n * name: ${pkg.name}\n * version: v${
-        pkg.version
-      }\n * (c) ${new Date().getFullYear()}\n * description: ${pkg.description}\n * author: ${pkg.author}\n *!/`,
-    }), */
     // Inspect(),
   ],
   test: {
     environment: 'jsdom',
     globals: true,
   },
-  /* build: {
-    // vite.config.ts
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'tres',
-      fileName: 'tres',
-      formats: ['es'],
-    },
-    watch: {
-      include: [resolve(__dirname, 'src')],
-    },
-    copyPublicDir: false,
-    rollupOptions: {
-      plugins: [
-        copy({
-          targets: [{ src: 'src/types/tres-components.d.ts', dest: 'dist/types' }],
-        }),
-        /!*   analyze(), *!/
-        /!* visualizer({
-          open: true,
-          gzipSize: true,
-          brotliSize: true,
-        }), *!/
-      ],
-
-      external: ['vue', '@vueuse/core', 'three'],
-      output: {
-        exports: 'named',
-      },
-    },
-  }, */
   optimizeDeps: {
     exclude: ['vue', 'three'],
   },
