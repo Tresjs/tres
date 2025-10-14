d26ac22e4f600d0d9f33bccd620b8a3f8ef1363e
lihbr <lihbr@users.noreply.github.com>
2025-10-14 22:52:09 +0200

    feat(cientos): support absolute-inspired positioning on `<ScreenSpace>`

--- apps/cientos-docs-vitepress/.vitepress/theme/components/ScreenSpaceDemo.vue
+++ apps/cientos-docs-vitepress/.vitepress/theme/components/ScreenSpaceDemo.vue
@@ -4,15 +4,46 @@ import { TresCanvas } from '@tresjs/core'
 </script>
 
 <template>
-  <TresCanvas clear-color="#333">
+  <TresCanvas>
     <TresPerspectiveCamera />
     <OrbitControls />
+
+    <!-- By default, children are centrered -->
     <ScreenSpace :depth="5">
       <TresMesh>
         <TresTorusGeometry />
         <TresMeshNormalMaterial />
       </TresMesh>
     </ScreenSpace>
+
+    <ScreenSpace :depth="8" :left="0.1" :top="0.2">
+      <TresMesh>
+        <TresTorusGeometry />
+        <TresMeshNormalMaterial />
+      </TresMesh>
+    </ScreenSpace>
+
+    <ScreenSpace :depth="10" :right="0" :bottom="0">
+      <TresMesh>
+        <TresTorusGeometry />
+        <TresMeshNormalMaterial />
+      </TresMesh>
+    </ScreenSpace>
+
+    <ScreenSpace :depth="25" :left="0.25" :bottom="0.25">
+      <TresMesh>
+        <TresTorusGeometry />
+        <TresMeshNormalMaterial />
+      </TresMesh>
+    </ScreenSpace>
+
+    <ScreenSpace :depth="15" :right="0.25">
+      <TresMesh>
+        <TresTorusGeometry />
+        <TresMeshNormalMaterial />
+      </TresMesh>
+    </ScreenSpace>
+
     <TresGridHelper :args="[10, 10]" />
   </TresCanvas>
 </template>
--- packages/cientos/src/core/abstractions/ScreenSpace.vue
+++ packages/cientos/src/core/abstractions/ScreenSpace.vue
@@ -1,22 +1,69 @@
 <script setup lang="ts">
-import { shallowRef } from 'vue'
-import { useLoop } from '@tresjs/core'
+import { computed, ref, shallowRef } from 'vue'
+import { logWarning, useLoop, useTresContext } from '@tresjs/core'
+import { OrthographicCamera, PerspectiveCamera } from 'three'
 
 export interface ScreenSpaceProps {
   depth?: number
+  top?: number
+  bottom?: number
+  left?: number
+  right?: number
 }
 
-withDefaults(defineProps<ScreenSpaceProps>(), {
+const props = withDefaults(defineProps<ScreenSpaceProps>(), {
   depth: -1,
 })
 
 const outerRef = shallowRef()
+const { camera, sizes } = useTresContext()
 
+const zoom = ref(1)
 useLoop().onBeforeRender(({ camera }) => {
-  if (outerRef.value && camera.value) {
-    outerRef.value.quaternion.copy(camera.value.quaternion)
-    outerRef.value.position.copy(camera.value.position)
+  if (!outerRef.value || !camera.value) {
+    return
   }
+
+  outerRef.value.quaternion.copy(camera.value.quaternion)
+  outerRef.value.position.copy(camera.value.position)
+
+  // Since `camera` is a shallow ref, we need to track zoom value on renders for `viewPlaneDimensions`
+  if (camera.value instanceof PerspectiveCamera || camera.value instanceof OrthographicCamera) {
+    zoom.value = camera.value.zoom
+  }
+})
+
+const viewPlaneDimensions = computed(() => {
+  let height: number = 0
+  let width: number = 0
+
+  const activeCamera = camera.activeCamera.value
+  if (activeCamera instanceof PerspectiveCamera) {
+    const fov = activeCamera.fov
+    height = (2 * Math.tan(fov * Math.PI / 180 / 2) * props.depth) / zoom.value
+    width = height * sizes.aspectRatio.value
+  }
+  else if (activeCamera instanceof OrthographicCamera) {
+    height = (activeCamera.top - activeCamera.bottom) / zoom.value
+    width = (activeCamera.right - activeCamera.left) / zoom.value
+  }
+  else if (activeCamera) {
+    logWarning(`ScreenSpace: Unhandled active camera type, only PerspectiveCamera and OrthographicCamera are supported when using \`top\`, \`bottom\`, \`left\`, and \`right\` props`, activeCamera)
+  }
+
+  return { height, width }
+})
+
+const innerPosition = computed<[number, number, number]>(() => {
+  // Default to center if no positioning specified
+  const leftPos = props.left ?? (props.right !== undefined ? 1 - props.right : 0.5)
+  const topPos = props.top ?? (props.bottom !== undefined ? 1 - props.bottom : 0.5)
+
+  const x = viewPlaneDimensions.value.width * (leftPos - 0.5)
+  const y = viewPlaneDimensions.value.height * (1 - topPos - 0.5) // Invert y so it behaves like CSS
+  const z = -props.depth
+
+  return [x, y, z]
 })
 
 defineExpose({ instance: outerRef })
@@ -24,7 +71,7 @@ defineExpose({ instance: outerRef })
 
 <template>
   <TresGroup ref="outerRef">
-    <TresGroup :position-z="-depth">
+    <TresGroup :position="innerPosition">
       <slot></slot>
     </TresGroup>
   </TresGroup>
