74ce2d84e76478bddbf98aed8bea2e67d368f44f
init-qy <953218204@qq.com>
2025-12-03 17:37:50 +0800

    perf(cientos): improve tree-shake for image and outline materials

--- packages/cientos/src/core/abstractions/Image/ImageMaterial.vue
+++ packages/cientos/src/core/abstractions/Image/ImageMaterial.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { shallowRef } from 'vue'
 import { extend } from '@tresjs/core'
-import ImageMaterial from './ImageMaterialImpl'
+import { imageMaterialImpl } from './ImageMaterialImpl'
 
-extend({ ImageMaterial })
+extend({ ImageMaterial: imageMaterialImpl })
 
 const materialRef = shallowRef()
 
