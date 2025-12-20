c478328bde9a5707f60a45b305ea82397dd21a1b
alvarosabu <alvaro.saburido@gmail.com>
2025-12-20 13:40:34 +0100

    feat(particles-morphing): integrate model loading and update shader parameters
    - Added loading functionality for a new GLTF model (blender-cube-simplified.glb).
    - Updated shader uniforms for resolution and size to enhance visual effects.
    - Refactored the template to utilize the new model particles, improving the overall particle morphing experience.

--- apps/lab/app/components/particles-morphing/Experiment.vue
+++ apps/lab/app/components/particles-morphing/Experiment.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import vertexShader from './shaders/vertex.glsl';
 import fragmentShader from './shaders/fragment.glsl';
-import { Uniform, Vector2, AdditiveBlending } from 'three';
+import { Uniform, Vector2, AdditiveBlending, SphereGeometry, MeshBasicMaterial, Float32BufferAttribute } from 'three';
 import { useDevicePixelRatio } from '@vueuse/core';
 
 const { sizes } = useTres()
 const { pixelRatio } = useDevicePixelRatio()
 const uniforms = {
   uTime: new Uniform(0),
-  uResolution: new Uniform(new Vector2(sizes.width.value * pixelRatio.value, sizes.height.value * pixelRatio.value)),
-  uSize: new Uniform(0.07),
+  uResolution: new Uniform(new Vector2(1 * pixelRatio.value, 1 * pixelRatio.value)),
+  uSize: new Uniform(50.0),
 }
 
 const geometryRef = ref<SphereGeometry | null>(null)
@@ -19,9 +19,29 @@ watch(geometryRef, (newGeometry) => {
     newGeometry.setIndex(null)
   }
 })
+
+// Load models
+
+const { isLoading, nodes } = useGLTF(
+  '/models/blender-cube-simplified.glb',
+  {
+    draco: true,
+  }
+)
+
+const blenderCube = computed(() => nodes.value?.BlenderCube)
+
+const modelParticlesPositions = computed(() => blenderCube.value?.geometry.attributes.position.array)
+const modelParticlesBufferAttribute = computed(() => new Float32BufferAttribute(modelParticlesPositions.value, 3))
+watch(modelParticlesBufferAttribute, (newModelParticlePositions) => {
+  if (newModelParticlePositions) {
+    console.log(modelParticlesBufferAttribute.value)
+  }
+})
 </script>
 <template>
-  <TresPoints>
+  <TresAmbientLight :intensity="1" />
+ <!--  <TresPoints>
     <TresSphereGeometry ref="geometryRef" :args="[1, 30, 30]" />
     <TresShaderMaterial 
     :vertex-shader="vertexShader" 
@@ -29,5 +49,14 @@ watch(geometryRef, (newGeometry) => {
     :uniforms="uniforms" 
     :transparent="true"
     :blending="AdditiveBlending" :depth-write="false"></TresShaderMaterial>
+  </TresPoints> -->
+  <TresPoints ref="modelParticlesPointsRef">
+    <TresBufferGeometry v-if="modelParticlesPositions" :position="[modelParticlesPositions, 3]" />
+    <TresShaderMaterial 
+    :vertex-shader="vertexShader" 
+    :fragment-shader="fragmentShader" 
+    :uniforms="uniforms" 
+    :transparent="true"
+    :blending="AdditiveBlending" :depth-write="false"></TresShaderMaterial>
   </TresPoints>
-</template>
+</template> 
