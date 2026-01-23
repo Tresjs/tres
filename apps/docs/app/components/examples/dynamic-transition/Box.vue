26810619cba0ed6915e471dc905d0d26b590ed54
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2026-01-23 21:43:01 +0000

    docs(core): Add Dynamic components recipe

---
+++ apps/docs/app/components/examples/dynamic-transition/Box.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { shallowRef } from 'vue'
+import { useLoop } from '@tresjs/core'
+
+const meshRef = shallowRef()
+
+const { onBeforeRender } = useLoop()
+
+onBeforeRender(({ elapsed }) => {
+  if (meshRef.value) {
+    meshRef.value.rotation.y = elapsed * 0.5
+    meshRef.value.rotation.x = elapsed * 0.2
+  }
+})
+</script>
+
+<template>
+  <TresMesh ref="meshRef">
+    <TresBoxGeometry :args="[1, 1, 1]" />
+    <TresMeshStandardMaterial color="orange" transparent />
+  </TresMesh>
+</template>
---
+++ apps/docs/app/components/examples/dynamic-transition/Sphere.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { shallowRef } from 'vue'
+import { useLoop } from '@tresjs/core'
+
+const meshRef = shallowRef()
+
+const { onBeforeRender } = useLoop()
+
+onBeforeRender(({ elapsed }) => {
+  if (meshRef.value) {
+    meshRef.value.position.y = Math.sin(elapsed) * 0.5
+    meshRef.value.position.x = Math.cos(elapsed) * 0.2
+  }
+})
+</script>
+
+<template>
+  <TresMesh ref="meshRef">
+    <TresSphereGeometry :args="[1, 32]" />
+    <TresMeshStandardMaterial color="orange" transparent />
+  </TresMesh>
+</template>
