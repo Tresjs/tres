5c3d386a6457cfc568b9400b077651c3c6e512b7
Tino Koch <tinoooo@users.noreply.github.com>
2026-02-04 11:28:12 +0100

    test(disposal): add test for disposing of mesh and geometry on parent component unmount

--- packages/core/src/tests/disposal.spec.ts
+++ packages/core/src/tests/disposal.spec.ts
@@ -1,4 +1,4 @@
-import { defineComponent, h, ref } from 'vue'
+import { defineComponent, h, nextTick, ref } from 'vue'
 import { createScene } from './utils/scene'
 import { beforeAll, describe, expect, it } from './utils/testing'
 import { isBufferGeometry, isMaterial, isMesh } from '../utils/is'
@@ -10,7 +10,7 @@ describe('disposal', () => {
     extend(THREE)
   })
 
-  it('should dispose of a mesh, geometry and renderer when unmounted', async () => {
+  it('should dispose of a mesh, geometry and renderer when a scene is unmounted', async () => {
     const exists = ref(true)
     const VForBoxes = defineComponent({
       setup: () => ({ exists }),
@@ -44,5 +44,45 @@ describe('disposal', () => {
     sceneWrapper.unmount()
 
     disposalSpies.forEach(spy => expect(spy).toHaveBeenCalledOnce())
+  })
+
+  it('should dispose of a mesh and geometry when the parent component is unmounted', async () => {
+    const exists = ref(true)
+    const Group = defineComponent({
+      setup: () => ({ exists }),
+      template: `
+      <TresGroup v-if="exists">
+        <TresMesh>
+          <TresBoxGeometry />
+          <TresMeshBasicMaterial />
+        </TresMesh>
+      </TresGroup>
+    `,
+    })
+    const { sceneWrapper, context } = await createScene(
+      () => h(Group),
+    )
+    const mesh = context.scene.value.children[0].children[0]
+
+    expect(isMesh(mesh)).toBe(true)
+    if (!isMesh(mesh)) { return }
+
+    expect(isBufferGeometry(mesh.geometry)).toBe(true)
+    if (!isBufferGeometry(mesh.geometry)) { return }
+
+    expect(isMaterial(mesh.material)).toBe(true)
+    if (!isMaterial(mesh.material)) { return }
+
+    const disposalSpies = [
+      mesh.geometry,
+      mesh.material,
+    ].map(item => vi.spyOn(item, 'dispose'))
+
+    exists.value = false
+    await nextTick()
+
+    disposalSpies.forEach(spy => expect(spy).toHaveBeenCalledOnce())
+
+    sceneWrapper.unmount()
   })
 })
