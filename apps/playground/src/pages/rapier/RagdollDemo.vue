3a8578736e838a343c262bd83c259e93550c5b22
Nathan Mande <neodarksoulink@gmail.com>
2026-02-27 17:15:50 +0300

    feat(rapier): ragdoll demo by @mattvb91
    ### Description
    
    Demo from @mattvb91 introduced on the old [TresJs-Rapier Repo](https://github.com/Tresjs/rapier/pull/160)
    
    Co-Authored-By: Matthias von Bargen <11991564+mattvb91@users.noreply.github.com>

---
+++ apps/playground/src/pages/rapier/RagdollDemo.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { OrbitControls } from '@tresjs/cientos'
+import { TresCanvas } from '@tresjs/core'
+import { TresLeches, useControls } from '@tresjs/leches'
+import { Physics, RigidBody } from '@tresjs/rapier'
+import { ACESFilmicToneMapping, SRGBColorSpace } from 'three'
+
+import Ragdoll from './components/Ragdoll.vue'
+
+const gl = {
+  clearColor: '#82DBC5',
+  shadows: true,
+  alpha: false,
+  outputColorSpace: SRGBColorSpace,
+  toneMapping: ACESFilmicToneMapping,
+}
+
+const { gravityY } = useControls({
+  gravityY: { value: -9.81, min: -20, max: 20, step: 0.1 },
+})
+</script>
+
+<template>
+  <TresLeches />
+  <TresCanvas v-bind="gl" window-size>
+    <TresPerspectiveCamera :position="[-4, 1, -4]" :look-at="[0, 0, 0]" />
+    <OrbitControls />
+
+    <Suspense>
+      <Physics debug :gravity="[0, gravityY, 0]">
+        <Ragdoll />
+
+        <RigidBody type="fixed" :position="[0, -1, 0]">
+          <TresMesh>
+            <TresPlaneGeometry :args="[20, 20, 20]" :rotate-x="-Math.PI / 2" />
+            <TresMeshBasicMaterial color="#f4f4f4" />
+          </TresMesh>
+        </RigidBody>
+      </Physics>
+    </Suspense>
+  </TresCanvas>
+</template>
