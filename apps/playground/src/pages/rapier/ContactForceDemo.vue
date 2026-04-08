111adece034a5ccb4e52f4ad03901c8f81ba590a
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2026-04-08 22:12:03 +0100

    docs(rapier): add contact-force docs & demo

--- apps/playground/src/pages/rapier/ContactForceDemo.vue
+++ apps/playground/src/pages/rapier/ContactForceDemo.vue
@@ -1,56 +1,59 @@
 <script setup lang="ts">
-import { OrbitControls } from "@tresjs/cientos";
-import { TresCanvas } from "@tresjs/core";
-import { TresLeches, useControls } from "@tresjs/leches";
-import { Physics, RigidBody, CuboidCollider } from "@tresjs/rapier";
-import { ACESFilmicToneMapping, SRGBColorSpace } from "three";
-import { shallowRef } from "vue";
+import { OrbitControls } from '@tresjs/cientos'
+import { TresCanvas } from '@tresjs/core'
+import { TresLeches, useControls } from '@tresjs/leches'
+import { CuboidCollider, Physics, RigidBody } from '@tresjs/rapier'
+import type { ContactForcePayload } from '@tresjs/rapier'
+import { ACESFilmicToneMapping, SRGBColorSpace } from 'three'
+import { shallowRef } from 'vue'
 
 const gl = {
-  clearColor: "#1a1a2e",
+  clearColor: '#1a1a2e',
   shadows: true,
   alpha: false,
   outputColorSpace: SRGBColorSpace,
   toneMapping: ACESFilmicToneMapping,
-};
+}
 
-const sphereRef = shallowRef();
+const sphereRef = shallowRef()
 
-const onContactForce = (event: any) => {
-  const magnitude = Math.round(event.totalForceMagnitude);
-  console.log("jaime ~ onContactForce ~ magnitude:", magnitude);
-};
-const onContactForceCustomCollider = (event: any) => {
-  const magnitude = Math.round(event.totalForceMagnitude);
-  console.log("jaime ~ onContactForceCustomCollider ~ magnitude:", magnitude);
-};
+const onContactForce = (event: ContactForcePayload) => {
+  const magnitude = Math.round(event.totalForceMagnitude)
+  // eslint-disable-next-line no-console
+  console.log('onContactForce ~ magnitude:', magnitude)
+}
+const onContactForceCustomCollider = (event: ContactForcePayload) => {
+  const magnitude = Math.round(event.totalForceMagnitude)
+  // eslint-disable-next-line no-console
+  console.log('onContactForceCustomCollider ~ magnitude:', magnitude)
+}
 
 const drop = () => {
   if (!sphereRef.value) {
-    return;
+    return
   }
-  sphereRef.value.instance.setTranslation({ x: 0, y: 10, z: 0 }, true);
-  sphereRef.value.instance.setLinvel({ x: 0, y: 0, z: 0 }, true);
-};
+  sphereRef.value.instance.setTranslation({ x: 0, y: 10, z: 0 }, true)
+  sphereRef.value.instance.setLinvel({ x: 0, y: 0, z: 0 }, true)
+}
 
 const { forceThreshold: FORCE_THRESHOLD, debug, activeContactForce } = useControls({
   debug: false,
   activeContactForce: true,
   forceThreshold: {
-    label: "Force threshold",
+    label: 'Force threshold',
     value: 10,
     min: 0,
     max: 25,
     step: 0.1,
   },
   acceptBtn: {
-    label: "Drop ball",
-    type: "button",
+    label: 'Drop ball',
+    type: 'button',
     onClick: () => {
-      drop();
+      drop()
     },
   },
-});
+})
 </script>
 
 <template>
@@ -69,23 +72,22 @@ const { forceThreshold: FORCE_THRESHOLD, debug, activeContactForce } = useContro
           ref="sphereRef"
           :active-contact-force="activeContactForce"
           :contact-force-event-threshold="FORCE_THRESHOLD"
-          @contact-force="onContactForce"
           collider="ball"
+          @contact-force="onContactForce"
         >
-          <
           <TresMesh :position="[0, 10, 0]" cast-shadow>
             <TresSphereGeometry :args="[1, 32, 32]" />
             <TresMeshStandardMaterial color="#e94560" />
           </TresMesh>
         </RigidBody>
 
-        <RigidBody  :collider="false">
+        <RigidBody :collider="false">
           <CuboidCollider
             :active-contact-force="activeContactForce"
             :contact-force-event-threshold="FORCE_THRESHOLD"
-            @contact-force="onContactForceCustomCollider"
             :args="[1, 1, 1]"
             :position="[0, 10, 0]"
+            @contact-force="onContactForceCustomCollider"
           />
           <TresMesh :position="[0, 10, 0]" cast-shadow>
             <TresSphereGeometry :args="[1, 32, 32]" />
