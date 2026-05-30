6174223148190e76e42b709e738649e29fffe42f
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2026-05-30 21:58:09 +0100

    fix(cientos):  closes #1407 - Fix the demo img (on playground), - Fix reactivity for the url prop - Improve the docs demo

--- apps/cientos-docs/app/components/objects/Image.vue
+++ apps/cientos-docs/app/components/objects/Image.vue
@@ -1,25 +1,34 @@
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core'
 import { Image } from '@tresjs/cientos'
-import { BackSide, DoubleSide, FrontSide } from 'three'
 import { useControls } from '@tresjs/leches'
 
 const uuid = inject(`uuid`)
 
+const URLS = [
+  'https://upload.wikimedia.org/wikipedia/commons/d/d4/Mars_2009_Plouaret.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/1/13/20220713-great-tit.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/0/00/Friendly_Robin.jpg',
+]
+
 const {
+  url,
   scaleX,
   scaleY,
   opacity,
   transparent,
   toneMapped,
-  side,
+  radius,
+  grayscale,
 } = useControls({
+  url: { value: URLS[0], options: URLS },
   scaleX: { value: 2, min: 0.1, max: 10, step: 0.1 },
   scaleY: { value: 2, min: 0.1, max: 10, step: 0.1 },
   opacity: { value: 1, min: 0, max: 1, step: 0.01 },
   transparent: false,
   toneMapped: true,
-  side: { value: DoubleSide, options: [FrontSide, BackSide, DoubleSide] },
+  radius: { value: 0, min: 0, max: 1, step: 0.01 },
+  grayscale: { value: 0, min: 0, max: 1, step: 0.01 },
 }, { uuid })
 </script>
 
@@ -28,12 +37,13 @@ const {
     <TresPerspectiveCamera :position="[0, 0, 5]" />
 
     <Image
-      url="https://upload.wikimedia.org/wikipedia/commons/f/f0/Cyanistes_caeruleus_Oulu_20150516.JPG"
+      :url="url"
       :scale="[scaleX, scaleY]"
       :opacity="opacity"
       :transparent="transparent"
       :tone-mapped="toneMapped"
-      :side="side"
+      :radius="radius"
+      :grayscale="grayscale"
     />
   </TresCanvas>
 </template>
