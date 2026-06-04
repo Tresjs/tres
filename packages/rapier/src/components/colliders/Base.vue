e3b66de4e053d7ae045e1265487aa65f19bb34fe
Nathan Mande <neodarksoulink@gmail.com>
2026-06-04 18:11:39 +0300

    fix(rapier): correct watchers redundancy
    ### Description
    
    - Address @alvarosabu [change request](https://github.com/Tresjs/tres/pull/1419#pullrequestreview-4418984904) and correct watchers redundancies

--- packages/rapier/src/components/colliders/Base.vue
+++ packages/rapier/src/components/colliders/Base.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
-import type { TresObject3D } from '@tresjs/core'
-import { ActiveCollisionTypes, ActiveEvents, ColliderDesc } from '@dimforge/rapier3d-compat'
-import { inject, nextTick, onUnmounted, type ShallowRef, shallowRef, watch } from 'vue'
+import { type TresObject3D } from '@tresjs/core'
+import {
+  ActiveCollisionTypes,
+  ActiveEvents,
+  ColliderDesc,
+  HeightFieldFlags
+} from '@dimforge/rapier3d-compat'
+import {
+  inject,
+  nextTick,
+  onUnmounted, type ShallowRef,
+  shallowRef,
+  watch
+} from 'vue'
 
 import { useRapierContext } from '../../composables'
 import { createCollider } from '../../core/collider'
-import { makePropsWatcherCL } from '../../utils'
+import { isVector3Like, makePropsWatcherCL } from '../../utils'
 import type { ColliderProps, CreateColliderReturnType, ExposedCollider, RigidBodyContext } from '../../types'
 
 const props = withDefaults(defineProps<Partial<ColliderProps>>(), {
@@ -32,26 +43,52 @@ const colliderInfos = shallowRef<CreateColliderReturnType>()
 const instance = shallowRef<CreateColliderReturnType['collider']>()
 const colliderDesc = shallowRef<CreateColliderReturnType['colliderDesc']>()
 
+const updateShapeArgs = (shape: Exclude<ColliderProps['shape'], undefined>, args: any[]) => {
+  const colliderDesc = ColliderDesc[shape]
+  const safeArgs: any[] = []
+
+  if (!colliderDesc) { return console.warn(`Invalid collider shape: ${shape}.`) }
+  if (!colliderInfos.value?.collider) { return }
+
+  if (
+    shape === "convexHull" ||
+    shape === "convexMesh" ||
+    shape === "polyline" ||
+    shape === "trimesh"
+  ) {
+    safeArgs.push(
+      args?.[0] instanceof Float32Array ? args?.[0] : new Float32Array(),
+      args?.[1] instanceof Uint32Array ? args?.[1] : new Uint32Array(),
+    )
+  } else if (shape === "heightfield") {
+    safeArgs.push(
+      typeof args?.[0] === 'number' ? args?.[0] : 1,
+      typeof args?.[1] === 'number' ? args?.[1] : 1,
+      args?.[2] instanceof Float32Array ? args?.[2] : new Float32Array(),
+      isVector3Like(args?.[3]) ? args?.[3] : { x: 0, y: 0, z: 0 }
+    )
+    if (args?.[4] in HeightFieldFlags) {
+      safeArgs.push(args?.[4])
+    }
+  } else {
+    safeArgs.push(
+      typeof args?.[0] === 'number' ? args?.[0] : 0.5,
+      typeof args?.[1] === 'number' ? args?.[1] : 0.5,
+      typeof args?.[2] === 'number' ? args?.[2] : 0.5,
+      typeof args?.[3] === 'number' ? args?.[3] : 0.5,
+    )
+  }
+
+  colliderInfos.value.collider.setShape(
+    (colliderDesc(...safeArgs as [any, any, any, any, any]))?.shape ?? ColliderDesc.cuboid(1, 1, 1).shape
+  )
+}
+
 defineExpose({
   instance,
   colliderDesc,
   object: colliderGroup,
 } satisfies { [K in keyof ExposedCollider]: ShallowRef<ExposedCollider[K] | undefined> })
-
-// TODO: collisionGroups
-makePropsWatcherCL(
-  props,
-  [
-    'friction',
-    'restitution',
-    'density',
-    'mass',
-    'activeCollisionTypes',
-    'activeCollision',
-    'sensor',
-  ],
-  colliderInfos,
-)
 
 watch(bodyContext, async (state) => {
   await nextTick()
@@ -86,25 +123,10 @@ watch(bodyContext, async (state) => {
 
 // Props watchers
 watch(() => props.shape, value => {
-  if (!colliderInfos.value?.collider) { return }
-  colliderInfos.value.collider.setShape(
-    (ColliderDesc[value](
-      props.args?.[0] ?? 1,
-      props.args?.[1] ?? 1,
-      props.args?.[2] ?? 1,
-      props.args?.[3] ?? 1,
-    ) ?? ColliderDesc.cuboid(1, 1, 1)
-    ).shape)
+  updateShapeArgs(value, props.args)
 })
 watch(() => props.args, value => {
-  if (!colliderInfos.value?.collider) { return }
-  colliderInfos.value.collider.setShape(
-    (ColliderDesc[props.shape](
-      value?.[0] ?? 1,
-      value?.[1] ?? 1,
-      value?.[2] ?? 1,
-      value?.[3] ?? 1,
-    ) ?? ColliderDesc.cuboid(1, 1, 1)).shape)
+  updateShapeArgs(props.shape, value)
 })
 watch(() => props.position, value => {
   if (!colliderInfos.value?.collider) { return }
@@ -123,36 +145,39 @@ watch(() => props.rotation, value => {
     w: typeof value?.[3] === 'number' ? value?.[3] : 1,
   })
 })
-watch(() => props.friction, value => colliderInfos.value?.collider.setFriction(value))
-watch(() => props.mass, value => colliderInfos.value?.collider.setMass(value))
-watch(() => props.restitution, value => colliderInfos.value?.collider.setRestitution(value))
-watch(() => props.density, value => colliderInfos.value?.collider.setDensity(value))
-watch(() => props.activeCollision, value => colliderInfos.value?.collider.setActiveEvents(value ? ActiveEvents.COLLISION_EVENTS : ActiveEvents.NONE))
-watch(() => props.activeCollisionTypes, value => colliderInfos.value?.collider.setActiveCollisionTypes(value))
-watch(() => props.collisionGroups, value => colliderInfos.value?.collider.setCollisionGroups(value ?? 0))
 watch(() => props.solverGroups, value => colliderInfos.value?.collider.setSolverGroups(value ?? 0))
-watch(() => props.sensor, value => colliderInfos.value?.collider.setSensor(value))
-watch(() => props.activeContactForce, value => colliderInfos.value?.collider.setActiveEvents(value ? ActiveEvents.CONTACT_FORCE_EVENTS : ActiveEvents.NONE))
-watch(() => props.contactForceEventThreshold, value => colliderInfos.value?.collider.setContactForceEventThreshold(value))
 watch([() => props.collisionGroups, colliderInfos], ([_collisionGroups, _]) => {
   if (!colliderInfos.value?.collider || !_collisionGroups) { return }
   colliderInfos.value.collider.setCollisionGroups(_collisionGroups)
 })
-watch([() => props.activeCollision, () => props.activeContactForce, colliderInfos], () => {
+watch([
+  () => props.activeCollision,
+  () => props.activeContactForce, colliderInfos
+], ([activeCollision, activeContactForce]) => {
   if (!colliderInfos.value?.collider) { return }
 
   let flags = ActiveEvents.NONE
+  if (activeCollision) { flags |= ActiveEvents.COLLISION_EVENTS }
+  if (activeContactForce) { flags |= ActiveEvents.CONTACT_FORCE_EVENTS }
 
-  if (props.activeCollision) { flags |= ActiveEvents.COLLISION_EVENTS }
-
-  if (props.activeContactForce) { flags |= ActiveEvents.CONTACT_FORCE_EVENTS }
   colliderInfos.value.collider.setActiveEvents(flags)
 })
 watch([() => props.contactForceEventThreshold, colliderInfos], ([threshold]) => {
   if (!colliderInfos.value?.collider || threshold === undefined) { return }
   colliderInfos.value.collider.setContactForceEventThreshold(threshold)
 })
-
+makePropsWatcherCL(
+  props,
+  [
+    'friction',
+    'restitution',
+    'density',
+    'mass',
+    'activeCollisionTypes',
+    'sensor',
+  ],
+  colliderInfos,
+)
 onUnmounted(() => {
   if (!bodyContext.value || !colliderInfos.value?.collider) { return }
 
